49cb3622bf106178cc810a1ec51fb776a717ca74
dev5@shayansolutions.com <shayansolutions@gmail.com>
2016-01-07 18:26:06 +0500

    updating modules

--- resources/views/sbticket/form.blade.php
+++ resources/views/sbticket/form.blade.php
@@ -227,80 +227,7 @@
 			</div>
 			
 												
-				
-
-	@if($subgrid['access']['is_add'] == '1')				
-	<hr /><div class="clr clear"></div>	
-	
-	<h5> get games on location </h5>
-	
-	<div class="table-responsive">
-	    <table class="table table-striped ">
-	        <thead>
-				<tr>
-					@foreach ($subgrid['tableGrid'] as $t)
-						@if($t['view'] =='1' && $t['field'] !='location_id')
-							<th>
-							{{ SiteHelpers::activeLang($t['label'],(isset($t['language'])? $t['language'] : array())) }}
-							</th>
-						@endif
-					@endforeach
-					<th></th>	
-				  </tr>
-
-	        </thead>
-
-        <tbody>
-        @if(count($subgrid['rowData'])>=1)
-            @foreach ($subgrid['rowData'] as $rows)
-	            <tr class="clone clonedInput">									
-					 @foreach ($subgrid['tableGrid'] as $field)
-						 @if($field['view'] =='1' && $field['field'] !='location_id')
-						 <td>					 
-						 	{!! SiteHelpers::bulkForm($field['field'] , $subgrid['tableForm'] , $rows->$field['field']) !!}							 
-						 </td>
-						 @endif					 
-					 
-					 @endforeach
-					 <td>
-					 	<a onclick=" $(this).parents('.clonedInput').remove(); return false" href="#" class="remove btn btn-xs btn-danger">-</a>
-					 	<input type="hidden" name="counter[]">
-					 </td>
-				</tr>  
-			@endforeach
-			
-
-		@else
-
-            <tr class="clone clonedInput">								
-			 @foreach ($subgrid['tableGrid'] as $field)
-
-				 @if($field['view'] =='1' && $field['field'] !='location_id')
-				 <td>					 
-				 	{!! SiteHelpers::bulkForm($field['field'] , $subgrid['tableForm'] ) !!}							 
-				 </td>
-				 @endif					 
-			 
-			 @endforeach
-				 <td>
-				 	<a onclick=" $(this).parents('.clonedInput').remove(); return false" href="#" class="remove btn btn-xs btn-danger">-</a>
-				 	<input type="hidden" name="counter[]">
-				 </td>
-			
-			</tr> 	
-		@endif	
-
-
-        </tbody>	
-
-     	</table>  
-    	<input type="hidden" name="enable-masterdetail" value="true">
-    </div><br /><br />
-     
-     <a href="javascript:void(0);" class="addC btn btn-xs btn-info" rel=".clone"><i class="fa fa-plus"></i> New Item</a>
-     <hr />		
-	@endif
-     					
+								
 						
 			<div style="clear:both"></div>	
 							
@@ -334,7 +261,7 @@ $(document).ready(function() {
         $("#game_id").jCombo("{{ URL::to('sbticket/comboselect?filter=game:location_id:game_name') }}",
         {  selected_value : '{{ $row["game_id"] }}' });
          
-	$('.addC').relCopy({});
+	
 	$('.editor').summernote();
 	$('.previewImage').fancybox();	
 	$('.tips').tooltip();	
