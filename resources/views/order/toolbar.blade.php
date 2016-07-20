29a5972647dabc2794214f5d945789a079c6bd78
arslan <shayansolutions@gmail.com>
2016-07-20 18:31:59 +0500

    set order

--- resources/views/order/toolbar.blade.php
+++ resources/views/order/toolbar.blade.php
@@ -41,14 +41,18 @@
                 @endif
 	</div>
 	<div class="col-md-4 ">
-		@if($access['is_excel'] ==1)
-		<div class="pull-right">
-			<a href="{{ URL::to( $pageModule .'/export/excel?return='.$return) }}" class="btn btn-sm btn-white"> Excel</a>
-			<a href="{{ URL::to( $pageModule .'/export/word?return='.$return) }}" class="btn btn-sm btn-white"> Word </a>
-			<a href="{{ URL::to( $pageModule .'/export/csv?return='.$return) }}" class="btn btn-sm btn-white"> CSV </a>
-			<a href="{{ URL::to( $pageModule .'/export/print?return='.$return) }}" class="btn btn-sm btn-white" onclick="ajaxPopupStatic(this.href); return false;" > Print</a>
-		</div>	
-		@endif
+        @if($access['is_excel'] ==1)
+            <div class="pull-right">
+        <a href="{{ URL::to( $pageModule .'/export/excel?return='.$return) }}" class="btn btn-sm btn-white">
+            Excel</a>
+        <a href="{{ URL::to( $pageModule .'/export/word?return='.$return) }}" class="btn btn-sm btn-white">
+            Word </a>
+        <a href="{{ URL::to( $pageModule .'/export/csv?return='.$return) }}" class="btn btn-sm btn-white">
+            CSV </a>
+        <a href="{{ URL::to( $pageModule .'/export/print?return='.$return) }}" class="btn btn-sm btn-white"
+           onclick="ajaxPopupStatic(this.href); return false;"> Print</a>
+    </div>
+    @endif
 	</div>
 </div>
 
