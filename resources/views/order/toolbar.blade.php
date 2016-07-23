ee1828bd361ade109455289b1b7a9e6dd066cd16
arslan <shayansolutions@gmail.com>
2016-07-23 18:09:29 +0500

    removed buttons

--- resources/views/order/toolbar.blade.php
+++ resources/views/order/toolbar.blade.php
@@ -40,18 +40,7 @@
                     @endif
                 @endif
 	</div>
-	<div class="col-md-4 ">
-        @if($access['is_excel'] ==1)
-            <div class="pull-right">
-        <a href="{{ URL::to( $pageModule .'/export/excel?return='.$return) }}" class="btn btn-sm btn-white">
-            Excel</a>
-        <a href="{{ URL::to( $pageModule .'/export/csv?return='.$return) }}" class="btn btn-sm btn-white">
-            CSV </a>
-        <a href="{{ URL::to( $pageModule .'/export/print?return='.$return) }}" class="btn btn-sm btn-white"
-           onclick="ajaxPopupStatic(this.href); return false;"> Print</a>
-    </div>
-    @endif
-	</div>
+
 </div>
 
 <script>
--- resources/views/pendingrequest/toolbar.blade.php
+++ resources/views/pendingrequest/toolbar.blade.php
@@ -18,9 +18,6 @@
 	<div class="col-md-4 ">
 		@if($access['is_excel'] ==1)
 		<div class="pull-right">
-			<a href="{{ URL::to( $pageModule .'/export/excel?return='.$return) }}" class="btn btn-sm btn-white"> Excel</a>
-			<a href="{{ URL::to( $pageModule .'/export/csv?return='.$return) }}" class="btn btn-sm btn-white"> CSV </a>
-			<a href="{{ URL::to( $pageModule .'/export/print?return='.$return) }}" class="btn btn-sm btn-white" onclick="ajaxPopupStatic(this.href); return false;" > Print</a>
 		</div>	
 		@endif
 	</div>
