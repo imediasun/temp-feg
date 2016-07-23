3d59b85264094ec207cf5a3e088fc9b1b721eeb4
arslan <shayansolutions@gmail.com>
2016-07-23 12:08:46 +0500

    remove word button

--- resources/views/gamestitle/toolbar.blade.php
+++ resources/views/gamestitle/toolbar.blade.php
@@ -36,8 +36,6 @@
             <div class="pull-right">
                 <a href="{{ URL::to( $pageModule .'/export/excel?return='.$return) }}" class="btn btn-sm btn-white">
                     Excel</a>
-                <a href="{{ URL::to( $pageModule .'/export/word?return='.$return) }}" class="btn btn-sm btn-white">
-                    Word </a>
                 <a href="{{ URL::to( $pageModule .'/export/csv?return='.$return) }}" class="btn btn-sm btn-white">
                     CSV </a>
                 <a href="{{ URL::to( $pageModule .'/export/print?return='.$return) }}" class="btn btn-sm btn-white"
