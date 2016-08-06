2e3f16039d2051ddb89c243d2b67a5730026810f
arslan <shayansolutions@gmail.com>
2016-08-06 13:09:06 +0500

    fix 198 issue

--- resources/views/managenewgraphicrequests/table.blade.php
+++ resources/views/managenewgraphicrequests/table.blade.php
@@ -88,7 +88,10 @@
 									 @elseif($field['field'] == 'need_by_date')
 
 										 {!! date("m/d/Y", strtotime($value)) !!}
-
+									 @elseif($field['field'] == 'approve_date')
+										 @if($value != '0000-00-00')
+											 {!! date("m/d/Y", strtotime($value)) !!}
+										 @endif
 									 @elseif($field['field'] == 'request_date')
 
 										 {!! date("m/d/Y", strtotime($value)) !!}
