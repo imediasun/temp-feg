467f936c40b9d0c00101feb480e2f187b8cd1e98
Sudipto Choudhury <mail@sudipto.net>
2016-08-24 04:14:27 +0530

    Report: Created module for GamePlay, ReadersWithMissingAssetId and MerchandizeExpense

---
+++ app/Models/Gameplayreport.php
@@ -0,0 +1,45 @@
+<?php namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class gameplayreport extends Sximo  {
+	
+	protected $table = 'game_earnings';
+	protected $primaryKey = 'id';
+
+	public function __construct() {
+		parent::__construct();
+		
+	}
+
+	public static function querySelect(  ){
+		
+		return "  SELECT game_earnings.* FROM game_earnings  ";
+	}	
+
+	public static function queryWhere(  ){
+		
+		return "  WHERE game_earnings.id IS NOT NULL ";
+	}
+	
+	public static function queryGroup(){
+		return "  ";
+	}
+	
+    public static function getSearchFilters() {
+        $finalFilter = array();
+        if (isset($_GET['search'])) {
+            $filters_raw = trim($_GET['search'], "|");
+            $filters = explode("|", $filters_raw);
+
+            foreach($filters as $filter) {
+                $columnFilter = explode(":", $filter);
+                if (isset($columnFilter) && isset($columnFilter[0]) && isset($columnFilter[2])) {
+                    $finalFilter[$columnFilter[0]] = $columnFilter[2];
+                }
+            }
+        }
+        return $finalFilter;
+    }
+}
