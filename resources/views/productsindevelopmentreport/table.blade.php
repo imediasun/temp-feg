1cd768eb8f962f02b4c5d557c3c4f53b4b391379
shayan.solutions <shayansolutions@gmail.com>
2016-08-30 14:07:17 +0500

    fixed date issue in B73

--- resources/views/productsindevelopmentreport/table.blade.php
+++ resources/views/productsindevelopmentreport/table.blade.php
@@ -89,11 +89,21 @@
 						 	<?php $limited = isset($field['limited']) ? $field['limited'] :''; ?>
 						 	@if(SiteHelpers::filterColumn($limited ))
 								 <td align="<?php echo $field['align'];?>" data-values="{{ $row->$field['field'] }}" data-field="{{ $field['field'] }}" data-format="{{ htmlentities($value) }}">
-									{!! $value !!}
+									 @if($field['field'] == 'ETA')
+
+
+										 {!! date("m/d/Y", strtotime($value)) !!}
+
+									 @else
+										 {!! $value !!}
+
+									 @endif
 								 </td>
 							@endif
                     <?php
 						 endif;
+
+
 						endforeach;
 					  ?>
                   @if($setting['disablerowactions']=='false')     
