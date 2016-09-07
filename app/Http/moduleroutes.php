434f32e98e11027a08bdc421a8f67c2c81f8b5e6
Sudipto Choudhury <mail@sudipto.net>
2016-09-08 01:24:37 +0530

    [routes] Fixing missing routes for reports which were removed in commit [a0d99e49d97bf9720eb1c8a970092baf29b95c20 - 7 Sep 2016]

--- app/Http/moduleroutes.php
+++ app/Http/moduleroutes.php
@@ -10,21 +10,16 @@
                     Route::controller('customer', 'CustomerController');
                     Route::controller('orderdetail', 'OrderdetailController');
                     Route::controller('product', 'ProductController');
-                    Route::controller('reports', 'ReportsController');
                     Route::controller('sbticket', 'SbticketController');
-                    Route::controller('topgame', 'TopgameController');
-                    Route::controller('bottomgame', 'BottomgameController');
                     Route::controller('department', 'DepartmentController');
                     Route::controller('location', 'LocationController');
                     Route::controller('game', 'GameController');
-                    Route::controller('gamesdown', 'GamesdownController');
                     Route::controller('ticketcomment', 'TicketcommentController');
                     Route::controller('gamestitle', 'GamestitleController');
                     Route::controller('order', 'OrderController');
                     Route::controller('pendingrequest', 'PendingrequestController');
                     Route::controller('tablecols', 'TablecolsController');
                     Route::controller('mylocationgame', 'MylocationgameController');
-                    Route::controller('merchandisebudget', 'MerchandisebudgetController');
                     Route::controller('shopfegrequeststore', 'ShopfegrequeststoreController');
                     Route::controller('addtocart', 'AddtocartController');
                     Route::controller('managefegrequeststore', 'ManagefegrequeststoreController');
@@ -41,13 +36,31 @@
                     Route::controller('managefreightquoters', 'ManagefreightquotersController');
                     Route::controller('freightquoters', 'FreightquotersController');
                     Route::controller('trainingmaterial', 'TrainingmaterialController');
-                    Route::controller('topgamesreport', 'TopgamesreportController');
-                    Route::controller('bottomgamesreport', 'BottomgamesreportController');
-                    Route::controller('productsindevelopmentreport', 'ProductsindevelopmentreportController');
-                    Route::controller('gameplayreport', 'GameplayreportController');
-                    Route::controller('readersmissingassetidreport', 'ReadersmissingassetidreportController');
-                    Route::controller('merchandiseexpensesreport', 'MerchandiseexpensesreportController');
                     Route::controller('itemreceipt', 'ItemreceiptController');
+                    
+                    // Routes for Reports [start]
+                    Route::controller('bottomgame', 'BottomgameController');                    // Bottom Games report - TODO: Not required, remove soon
+                    Route::controller('bottomgamesreport', 'BottomgamesreportController');      // Bottom Games report - TODO: Not required, remove soon
+                    Route::controller('bottomgamesavgplays', 'BottomgamesavgplaysController');  // Bottom Games by average report - TODO: Not required, remove soon
+                    Route::controller('closedlocations', 'ClosedlocationsController');          // Cloased Locations report
+                    Route::controller('gameplayreport', 'GameplayreportController');            // Game wise Game Play report
+                    Route::controller('gameplayrankbylocation', 'GameplayrankbylocationController');// Location wise game play report
+                    Route::controller('gamesdown', 'GamesdownController');                      // Games down report
+                    Route::controller('gamesnotondebitcard', 'GamesnotondebitcardController');  // Games not on debit card report
+                    Route::controller('gamesnotplayed', 'GamesnotplayedController');            // Games not played report
+                    Route::controller('topgamesreport', 'TopgamesreportController');            // Game Title wise game play report
+                    Route::controller('reports', 'ReportsController');                          // Location not reporting report
+                    Route::controller('merchthrowsdetailed', 'MerchthrowsdetailedController');  // Merch throws deailed report (old but required)
+                    Route::controller('merchthrowssimple', 'MerchthrowssimpleController');      // Merch throws simple report (old but required)
+                    Route::controller('merchandisebudget', 'MerchandisebudgetController');      // Merch budget report - (old but required)
+                    Route::controller('merchandiseexpensesreport', 'MerchandiseexpensesreportController');// Merch expenses report - (old but required)
+                    Route::controller('potentialoverreportingerrors', 'PotentialoverreportingerrorsController');// Potential over reporting error report
+                    Route::controller('productusagereport', 'ProductusagereportController');    // Product Usage report
+                    Route::controller('productsindevelopmentreport', 'ProductsindevelopmentreportController');// Products in development report
+                    Route::controller('nonfegreaders', 'NonfegreadersController');              // Non FEG Readers report
+                    Route::controller('readersmissingassetidreport', 'ReadersmissingassetidreportController');// Readers with missing asset ids report
+                    Route::controller('topgame', 'TopgameController');                          // top games report - TODO: Not required, remove soon
+                    Route::controller('topgamesavgplays', 'TopgamesavgplaysController');        // top games report by average - TODO: Not required, remove soon
                     Route::controller('throwreport', 'ThrowreportController');
-
+                    // Routes for Reports [end]
                     ?>
