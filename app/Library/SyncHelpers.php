2365a752dcc04bed8a0f718486c7e8e35e196f6b
Sudipto Choudhury <mail@sudipto.net>
2016-09-23 06:22:30 +0530

    [sync live to dev] earnings sync fixes#7

--- app/Library/SyncHelpers.php
+++ app/Library/SyncHelpers.php
@@ -71,7 +71,7 @@ class SyncHelpers
             unset($item['id']);
             unset($item['date_start']);
             unset($item['loc_id']); 
-            DB::table($table)->where('id', $id)->update($item);
+            DB::table($table)->where('id', '=', $id)->update($item);
         }
         DB::commit();
     }
@@ -100,7 +100,7 @@ class SyncHelpers
         $data = $live_db->select($q);
         DB::beginTransaction();
         foreach($data as $item) {
-            DB::table($table)->where([['location_id', $item['location_id']],['date_played', $item['date_played']]])->update(["record_status" => 0]);
+            DB::table($table)->where([['location_id', '=', $item['location_id']],['date_played', '=', $item['date_played']]])->update(["record_status" => 0]);
             DB::table($table)->insert($item);
         }
         DB::commit();
@@ -119,7 +119,7 @@ class SyncHelpers
         $data = $live_db->select($q);
         DB::beginTransaction();
         foreach($data as $item) {
-            DB::table($table)->where([['game_id', $item['game_id']],['date_played', $item['date_played']]])->update(["record_status" => 0]);
+            DB::table($table)->where([['game_id', '=', $item['game_id']],['date_played', '=', $item['date_played']]])->update(["record_status" => 0]);
             DB::table($table)->insert($item);
         }
         DB::commit();
