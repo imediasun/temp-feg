70ba44286505234e1a0164d92433edf112b84cb1
Arslan Ali <shayansolutions@gmail.com>
2016-09-26 16:22:30 +0500

    [Config] updating location and vendor for row# and checkbox

--- resources/views/location/table.blade.php
+++ resources/views/location/table.blade.php
@@ -88,7 +88,9 @@ if (!$colconfigs) {
                     @if($access['is_add'] =='1' && $setting['inline']=='true')
                         <tr id="form-0">
                             <td> #</td>
+                            @if($setting['disableactioncheckbox']=='false')
                             <td></td>
+                            @endif
                             @if($setting['view-method']=='expand')
                                 <td></td> @endif
                             @foreach ($tableGrid as $t)
@@ -114,9 +116,12 @@ if (!$colconfigs) {
                     ?>
 
                     <tr class="editable" id="form-{{ $row->id }}">
+                        @if(!isset($setting['hiderowcountcolumn']) || $setting['hiderowcountcolumn'] != 'true')
                         <td class="number"> <?php echo ++$i;?>  </td>
+                        @endif
+                        @if($setting['disableactioncheckbox']=='false')
                         <td><input type="checkbox" class="ids" name="ids[]" value="<?php echo $row->id;?>"/></td>
-
+                        @endif
                         @if($setting['view-method']=='expand')
                             <td><a href="javascript:void(0)" class="expandable" rel="#row-{{ $row->id }}"
                                    data-url="{{ url('location/show/'.$id) }}"><i class="fa fa-plus "></i></a></td>
