83cf1b05fc30d0696edb967e51d2f38bcbe6c1cd
Sudipto Choudhury <mail@sudipto.net>
2016-09-27 08:38:13 +0530

    [simple search] image gallery added clear search and reload data buttons

--- resources/views/redemptioncountergallary/table.blade.php
+++ resources/views/redemptioncountergallary/table.blade.php
@@ -2,6 +2,10 @@
 <div class="sbox">
     <div class="sbox-title">
         <h5> <i class="fa fa-table"></i>  {{ $pageTitle }}</h5>
+		<div class="sbox-tools" >
+			<a href="javascript:void(0)" class="btn btn-xs btn-white tips" title="Clear Search" onclick="reloadData('#{{ $pageModule }}','{{ $pageModule }}/data?search=')"><i class="fa fa-trash-o"></i> Clear Search </a>
+			<a href="javascript:void(0)" class="btn btn-xs btn-white tips" title="Reload Data" onclick="reloadData('#{{ $pageModule }}','{{ $pageModule }}/data?return={{ $return }}')"><i class="fa fa-refresh"></i></a>
+		</div>        
     </div>
     <div class="sbox-content">
         @if($setting['usesimplesearch']!='false')     
