83d014d2e45901ea69874c87a28ab7cec1d05e42
shayan.solutions <shayansolutions@gmail.com>
2016-09-28 17:03:34 +0500

    ADD TICKETSETTING ROUTUE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -65,10 +65,7 @@ Route::filter('authorization', function()
 
 });
 Route::get('submitservicerequest/{GID?}/{LID?}', 'SubmitservicerequestController@getIndex');
-
-
-
-
+Route::get('ticketsetting','SbticketController@getSetting');
 Route::get('order/submitorder/{SID?}', 'OrderController@getSubmitorder');
 Route::get('/', 'UserController@getLogin');
 Route::controller('home', 'HomeController');
