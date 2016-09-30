6a5108cfa3cbd11354e69734e4f95cfd2f3d3303
Sudipto Choudhury <mail@sudipto.net>
2016-10-01 04:10:53 +0530

    [feg tasks] feg tasks fix9

--- resources/views/feg/system/tasks/table.blade.php
+++ resources/views/feg/system/tasks/table.blade.php
@@ -6,7 +6,7 @@
         </div>
 	</div>
 	<div class="sbox-content">
-	 <?php echo Form::open(array('url'=>'{{ $pageModule }}/delete/', 'class'=>'form-horizontal' ,'id' =>'SximoTable'  ,'data-parsley-validate'=>'' )) ;?>
+	 <?php echo Form::open(array('url'=>'tasks/delete/', 'class'=>'form-horizontal' ,'id' =>'SximoTable'  ,'data-parsley-validate'=>'' )) ;?>
 <div class="table-responsive">
     @if(!empty($topMessage))
     <h5 class="topMessage">{{ $topMessage }}</h5>
@@ -90,7 +90,7 @@
 					<td ><input type="checkbox" class="ids" name="ids[]" value="<?php echo $row->id ;?>" />  </td>
                     @endif
 					@if($setting['view-method']=='expand')
-					<td><a href="javascript:void(0)" class="expandable" rel="#row-{{ $row->id }}" data-url="{{ url('{{ $pageModule }}/show/'.$id) }}"><i class="fa fa-plus " ></i></a></td>
+					<td><a href="javascript:void(0)" class="expandable" rel="#row-{{ $row->id }}" data-url="{{ url('tasks/show/'.$id) }}"><i class="fa fa-plus " ></i></a></td>
 					@endif
 					 <?php foreach ($tableGrid as $field) :
 					 	if($field['view'] =='1') :
@@ -111,7 +111,7 @@
 					  ?>
                   @if($setting['disablerowactions']=='false')     
 				 <td data-values="action" data-key="<?php echo $row->id ;?>">
-					{!! AjaxHelpers::buttonAction('{{ $pageModule }}',$access,$id ,$setting) !!}
+					{!! AjaxHelpers::buttonAction('tasks',$access,$id ,$setting) !!}
 					{!! AjaxHelpers::buttonActionInline($row->id,'id') !!}
 				</td>
                 @endif
