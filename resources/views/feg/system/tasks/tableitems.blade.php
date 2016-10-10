ddf019ddcf9871775be473f108cea713e7a8a307
Sudipto Choudhury <mail@sudipto.net>
2016-10-10 14:19:22 +0530

    [feg tasks] more task settings #6

--- resources/views/feg/system/tasks/tableitems.blade.php
+++ resources/views/feg/system/tasks/tableitems.blade.php
@@ -63,16 +63,14 @@
                     <div class="clearfix cronscheduletext">
                         <strong>Schedule: </strong><span class="cronStampText" data-cronstamp="{{ $schedule }}"></span> 
                     </div>
-                    @if (!empty($run_before) || !empty($run_after))
-                    <div class="textContent clearfix m-t">
-                        @if (!empty($run_before))
-                        <p><strong>Run Before: </strong><span class="label cronscheduleBeforeText" data-runBeforeTask="{{ $run_before }}"></span> </p>
+                    <div class="textContent clearfix m-t taskAttachBeforeAfter @if (empty($run_before) && empty($run_after)) hidden @endif">                        
+                        <p class="@if (empty($run_before)) hidden @endif"
+                            ><strong>Run Before: </strong><span class="label cronscheduleBeforeText" data-runBeforeTask="{{ $run_before }}"></span> </p>
                         @endif
-                        @if (!empty($run_after))
-                        <p><strong>Run After: </strong><span class="label cronscheduleAfterText" data-runAfterTask="{{ $run_after }}"></span> </p>
+                        <p class="@if (empty($run_after)) hidden @endif"
+                           ><strong>Run After: </strong><span class="label cronscheduleAfterText" data-runAfterTask="{{ $run_after }}"></span> </p>
                         @endif
                     </div>
-                    @endif
                     <div class="formContent hidden clearfix m-t">
                         <div class="form-inline clearfix cronscheduleinputs">
                             <label><strong>Edit: </strong></label>
