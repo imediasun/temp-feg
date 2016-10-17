0901afdc60761f5f8c49eaa69087f0aaae894d03
Arslan Ali <shayansolutions@gmail.com>
2016-10-17 16:17:15 +0500

    [Users] fixing separator for location

--- app/Models/Core/Users.php
+++ app/Models/Core/Users.php
@@ -17,7 +17,7 @@ class Users extends Sximo  {
 	public static function querySelect(  ){
 		
 		return " SELECT  users.*,  tb_groups.name, users.group_id, users.username,
-                IF(has_all_locations = 0,(SELECT GROUP_CONCAT(location_name) FROM user_locations JOIN location ON location.id = user_locations.location_id WHERE user_id = users.id GROUP BY user_id) ,\"All\") AS has_all_locations
+                IF(has_all_locations = 0,(SELECT GROUP_CONCAT(location_name SEPARATOR ', ') FROM user_locations JOIN location ON location.id = user_locations.location_id WHERE user_id = users.id GROUP BY user_id) ,\"All Locations\") AS has_all_locations
                 FROM users LEFT JOIN tb_groups ON tb_groups.group_id = users.group_id ";
 	}	
 
