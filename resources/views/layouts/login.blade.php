6638891cfd8e7300f6e77055a8b6bfc3d403add2
shayansolutions <shayansolutions@gmail.com>
2016-12-08 12:15:05 +0500

    [front-end][login-bg]

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -26,7 +26,7 @@
     <link href="{{ asset('sximo/css/feg_new_styles.css')}}" rel="stylesheet">
 	
   	</head>
-<body id="login-bg">
+<body id="login-bg" style="background:url('{{asset("sximo/images/feg-login-bg.jpg") }}');background-repeat:no-repeat;background-size: 100%">
     <div class="middle-box  ">
         <div>
 
