e3631e124688314a53bde96a18a6a5fea183ebfe
Sudipto Choudhury <mail@sudipto.net>
2016-12-26 04:10:32 +0530

    [daily reports]  table border fix

--- app/Library/FEG/System/Email/ReportGenerator.php
+++ app/Library/FEG/System/Email/ReportGenerator.php
@@ -919,11 +919,12 @@ class ReportGenerator
         $top25GamesThisWeekData = DB::select($q);
         $topGamesTable = array(); 
         $blackBrdStyle = "style='border:1px solid black;'";
-        $greyBrdStyle = "style='border:1px solid silver;'";        
+        $greyBrdStyle = "style='border:1px solid silver; padding: 3px;'";        
+        $greyBrdTrStyle = "style='border:1px solid silver;'";        
         if (!empty($top25GamesThisWeekData)) {
             $topGamesTable[] = "<table style='margin:0px auto; 
                 width:100%; border:1px solid white; color:black;'>";
-            $topGamesTable[] = "<tr $blackBrdStyle>
+            $topGamesTable[] = "<tr $greyBrdTrStyle>
                     <th $blackBrdStyle>No.</th>
                     <th $blackBrdStyle width='200'>Game</th>
                     <th $blackBrdStyle>Average</th>
