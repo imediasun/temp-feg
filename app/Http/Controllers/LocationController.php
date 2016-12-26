a5c3a111e24ecbee5543aec178ad0cb02bf8076b
shayansolutions <shayansolutions@gmail.com>
2016-12-26 23:09:51 +0500

    testing ticket comment modifications

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -381,7 +381,7 @@ function getIsLocationAvailable($id)
                //insert comment
                 $postUser = \DB::select("Select * FROM users WHERE email = '". $from ."'");
                 $userId = $postUser[0]->id;
-                $message = imap_fetchbody($inbox,$email_number,1.2);
+                $message = imap_fetchbody($inbox,$email_number,1.1);
 
                 //Insert In sb_comment table
                $comment_model = new Ticketcomment();
