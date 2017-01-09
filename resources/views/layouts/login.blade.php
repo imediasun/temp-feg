2c9025c4c7822d5a8070dcccfaab7fab390aaad3
arslan <shayansolutions@gmail.com>
2017-01-09 17:39:04 +0500

    [Backend][AddBackgroundProperty]

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -26,7 +26,7 @@
     <link href="{{ asset('sximo/css/feg_new_styles.css')}}" rel="stylesheet">
 	
   	</head>
-<body id="login-bg" style="background:url('{{asset("sximo/images/feg-login-bg.jpg") }}');background-size: 100% auto;background-attachment: fixed;background-position: center top;">
+<body id="login-bg" style="background:url('{{asset("sximo/images/feg-login-bg.jpg") }}');background-size: 100% auto;background-attachment: fixed;background-position: center top;background-size: cover; background-color:#003673">
     <div class="middle-box  ">
         <div>
 
