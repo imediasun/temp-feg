e2c2f917ed25c39171d6a049532b347d37bb4eea
shayansolutions <shayansolutions@gmail.com>
2017-01-16 14:15:20 +0500

    add video link in Tech Training

--- app/Http/moduleroutes.php
+++ app/Http/moduleroutes.php
@@ -59,5 +59,5 @@ Route::controller('throwreportinstantwin', 'ThrowreportinstantwinController');
 Route::controller('servicerequests', 'ServicerequestsController');
 Route::controller('feg/system/tasks', 'Feg\System\TasksController');
 Route::controller('feg/system/systememailreportmanager', 'Feg\System\SystemEmailReportManagerController');
-//Route::controller('training', 'TrainingController');
+Route::controller('training', 'TrainingController');
 ?>
