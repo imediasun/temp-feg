e9524ce3c61afb186f44e29b6ae8d164e24a2908
adnan <shayansolutions@gmail.com>
2017-01-20 08:58:56 +0500

    [front-end][alignment issues in gamesintransit]

--- resources/views/gamesintransit/table.blade.php
+++ resources/views/gamesintransit/table.blade.php
@@ -73,7 +73,7 @@
                 endif;
             endforeach; ?>
             @if($setting['disablerowactions']=='false')
-                <th width="170"><?php echo Lang::get('core.btn_action') ;?></th>
+                <th width="70"><?php echo Lang::get('core.btn_action') ;?></th>
             @endif
         </tr>
         </thead>
@@ -138,7 +138,7 @@
 						 endif;
 						endforeach;
 					  ?>
-				 <td data-values="action" data-key="<?php echo $row->id ;?>">
+				 <td class="action"  data-values="action" data-key="<?php echo $row->id ;?>">
 					{!! AjaxHelpers::buttonAction('gamesintransit',$access,$id ,$setting) !!}
 					{!! AjaxHelpers::buttonActionInline($row->id,'id') !!}
 				</td>
