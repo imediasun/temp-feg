2c58dea91b5fc05491b0c3dcf958a98d8201ccaa
adnan <shayansolutions@gmail.com>
2017-01-20 15:10:47 +0500

    [front-end][alignment in freight tool]

--- resources/views/managefreightquoters/table.blade.php
+++ resources/views/managefreightquoters/table.blade.php
@@ -75,8 +75,8 @@
                 endif;
             endforeach; ?>
 
-                <th width="250">Freight Company</th>
-                <th width="300">Description</th>
+                <th width="130">Freight Company</th>
+                <th width="150">Description</th>
                 @if($setting['disablerowactions']=='false')
                     <th width="50"><?php echo Lang::get('core.btn_action') ;?></th>
                 @endif
