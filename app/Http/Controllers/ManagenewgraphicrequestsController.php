41b7cfca29816469e238b746b467abc6e6965d8b
Arslan Ali <shayansolutions@gmail.com>
2017-02-06 19:36:45 +0500

    [GraphicRequest] manage graphic request status ordering

--- app/Http/Controllers/ManagenewgraphicrequestsController.php
+++ app/Http/Controllers/ManagenewgraphicrequestsController.php
@@ -295,4 +295,39 @@ class ManagenewgraphicrequestsController extends Controller
         }
     }
 
+    function getComboselect(Request $request)
+    {
+        if ($request->ajax() == true && \Auth::check() == true) {
+            $param = explode(':', $request->input('filter'));
+            $parent = (!is_null($request->input('parent')) ? $request->input('parent') : null);
+
+            $limit = (!is_null($request->input('limit')) ? $request->input('limit') : null);
+            $delimiter = empty($request->input('delimiter')) ? ' ' : $request->input('delimiter');
+
+            $rows = \DB::table('new_graphics_request_status')->orderBy('sort','asc')->get();
+
+            $items = array();
+
+            $fields = explode("|", $param[2]);
+
+            foreach ($rows as $row) {
+                $value = "";
+                $values = array();
+                foreach ($fields as $item => $val) {
+                    if ($val != "") {
+                        $values[] = $row->$val;
+                    }
+                    $value = implode($delimiter, $values);
+                }
+                $items[] = array($row->$param['1'], $value);
+
+            }
+
+            return json_encode($items);
+        } else {
+            return json_encode(array('OMG' => " Ops .. Cant access the page !"));
+        }
+    }
+
+
 }
