bad7544d17eddcd1f636da2be76602378f192dfc
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-02-09 18:10:33 +0530

    [css] [template] [My Locations games] Position the toolbar buttons

--- resources/views/mylocationgame/toolbar.blade.php
+++ resources/views/mylocationgame/toolbar.blade.php
@@ -38,7 +38,8 @@
    </form>
 </div>
 <div class="clearfix m-b">
-    <div class="pull-left">
+    <div class="pull-left form-inline">
+        <div class="pull-left m-r-f" >
         @if($access['is_add'] ==1)
             {!! AjaxHelpers::buttonActionCreate($pageModule,$setting) !!}
             <a href="javascript://ajax" class="btn btn-sm btn-white" onclick="ajaxCopy('#{{ $pageModule }}','{{ $pageUrl }}')"><i class="fa fa-file-o"></i> Copy </a>
@@ -47,10 +48,12 @@
             <a href="javascript://ajax" class="btn btn-sm btn-white" onclick="ajaxRemove('#{{ $pageModule }}','{{ $pageUrl }}');"><i class="fa fa-trash-o "></i> {{ Lang::get('core.btn_remove') }} </a>
         @endif
         <a href="{{ URL::to( $pageModule .'/search') }}" class="btn btn-sm btn-white" onclick="SximoModal(this.href,'Advanced Search'); return false;" ><i class="fa fa-search"></i>Advanced Search</a>
+        </div>
         @if(SiteHelpers::isModuleEnabled($pageModule))
+            <div class="pull-left">
             <a href="{{ URL::to('tablecols/arrange-cols/'.$pageModule) }}" class="btn btn-sm btn-white" onclick="SximoModal(this.href,'Column Selector'); return false;" ><i class="fa fa-bars"></i> Arrange Columns</a>
             @if(!empty($colconfigs))
-                <select class="form-control" style=" width:25%!important;display:inline;" name="col-config"
+                <select class="form-control" name="col-config"
                         id="col-config">
                     <option value="0">Select Configuraton</option>
                     @foreach($colconfigs as $configs )
@@ -58,12 +61,13 @@
                                                                          @endif value={{ $configs['config_id'] }}> {{ $configs['config_name'] }}   </option>
                     @endforeach
                 </select>
-                    @if(\Session::get('uid') ==  \SiteHelpers::getConfigOwner($config_id))
+                    @if(\Session::get('uid') ==  \SiteHelpers::getConfigOwner($config_id))                    
                         <a id="edit-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule.'/edit') }}" class="btn btn-sm btn-white tips"
                            onclick="SximoModal(this.href,'Column Selector'); return false;" title="Edit Arrange">  <i class="fa fa-pencil-square-o"></i></a>
-                        <button id="delete-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule.'/delete') }}" class="btn btn-sm btn-white tips" title="Clear Arrange">  <i class="fa fa-trash-o"></i></button>
+                        <button id="delete-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule.'/delete') }}" class="btn btn-sm btn-white tips" title="Clear Arrange">  <i class="fa fa-trash-o"></i></button>                    
                     @endif
-                @endif
+            @endif
+            </div>
         @endif
     </div>
     <div class="pull-right">
