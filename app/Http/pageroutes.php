70969f7283e60c20900e26fc7409c38fddbfb186
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-02-09 17:17:22 +0530

    Revert "[css] [core] classes for line hieghts (em) and tiny margins"
    This reverts commit 0483dbf710e8fa4bce27e111074bea2e5fafb7e4.

--- app/Http/pageroutes.php
+++ app/Http/pageroutes.php
@@ -1,4 +1,10 @@
 <?php 
+Route::get('contact-us', 'HomeController@index');
+Route::get('about-us', 'HomeController@index');
+Route::get('service', 'HomeController@index');
+Route::get('faq', 'HomeController@index');
+Route::get('portpolio', 'HomeController@index');
+Route::get('tickets', 'HomeController@index');
 Route::get('holidays', 'HomeController@index');
 Route::get('preemployment', 'HomeController@index');
 Route::get('currentemployees', 'HomeController@index');
@@ -7,7 +13,7 @@ Route::get('newhires', 'HomeController@index');
 Route::get('independentcontractors', 'HomeController@index');
 Route::get('terminate', 'HomeController@index');
 Route::get('gamemaintenance', 'HomeController@index');
-Route::get('training-old', 'HomeController@index');
+Route::get('training', 'HomeController@index');
 Route::get('shippinggames', 'HomeController@index');
 Route::get('inventor', 'HomeController@index');
 Route::get('generaltips', 'HomeController@index');
