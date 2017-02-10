e2ac9473ee598ddc6f3bddbcc75b0c482e11ec89
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-02-10 07:22:14 +0530

    [my location game] Test different download options for asset tag image file #2

--- app/Http/Controllers/MylocationgameController.php
+++ app/Http/Controllers/MylocationgameController.php
@@ -924,7 +924,8 @@ class MylocationgameController extends Controller
                 $file = storage_path() . '/qr/' . $asset_ids . '.png';
                 if (file_exists($file)) {
                     $zip->addFile($file, basename($file));
-                    return response()->download($file, $asset_ids.'.png', array( 'Content-Type' => 'image/png' ))->setContentDisposition('inline');
+                    return response()->download($file, $asset_ids.'.png', array( 'Content-Type' => 'image/png' ))
+                            ->setContentDisposition('attachment; filename='.$asset_ids.'.png');
                 } else {
                     die('file does not exists');
                 }
