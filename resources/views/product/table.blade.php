335acfe4b03b463d954669f84c5c452e89d8638e
Awais <shayansolutions@gmail.com>
2017-02-15 17:09:32 +0500

    [enable inline editing in all modules]

--- resources/views/product/table.blade.php
+++ resources/views/product/table.blade.php
@@ -86,7 +86,7 @@
 				@endif
 				@if($setting['view-method']=='expand') <td> </td> @endif
 				@foreach ($tableGrid as $t)
-					@if($t['inline'] =='1')
+					@if(isset($t['inline']) && $t['inline'] =='1')
 					<?php $limited = isset($t['limited']) ? $t['limited'] :''; ?>
 						@if(SiteHelpers::filterColumn($limited ))
 						<td data-form="{{ $t['field'] }}" data-form-type="{{ AjaxHelpers::inlineFormType($t['field'],$tableForm)}}">
