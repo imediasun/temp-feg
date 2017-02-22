e7978bc5f474ec35610a255101cbaaee6e819a15
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-02-22 12:42:44 +0530

    [page cms] Enhance grid with sortable columns and better sized columns
    [core] Show a prompt from Page CMS module when view is restricted for the user (instead of redirecting to dashboard) This is not useful though in this module but will be used later to show a prompt instead of redirecting to dashboard when menus are opned from sidebar menu (will be implemented later)

--- app/Http/Controllers/Core/PagesController.php
+++ app/Http/Controllers/Core/PagesController.php
@@ -61,9 +61,10 @@ class PagesController extends Controller
 
         // Build pagination setting
         $page = $page >= 1 && filter_var($page, FILTER_VALIDATE_INT) !== false ? $page : 1;
-        if($params['limit'] == 0)
-            $params['limit'] = $results['total'];
-        $pagination = new Paginator($results['rows'], $results['total'], $params['limit']);
+        //$pagination = new Paginator($results['rows'], $results['total'], $params['limit']);
+        $pagination = new Paginator($results['rows'], $results['total'],
+            (isset($params['limit']) && $params['limit'] > 0 ? $params['limit'] :
+                ($results['total'] > 0 ? $results['total'] : '1')));
         $pagination->setPath('pages');
 
         $this->data['param'] = $params;
