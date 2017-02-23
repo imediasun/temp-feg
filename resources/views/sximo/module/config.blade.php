7ea435b65f72b98659c6a795cf395481f716423b
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-02-23 11:59:10 +0530

    [module] [core] add module setting to set module route for those routes like core/users core/groups, feg/system/tasks etc. having path deep path [need to add in demo]

--- resources/views/sximo/module/config.blade.php
+++ resources/views/sximo/module/config.blade.php
@@ -125,7 +125,12 @@
   <input  type='text' name='module_id' id='module_id'  value='{{ $row->module_id }}'  style="display:none; " />
   	<fieldset>
 		<legend> Module Setting </legend>
-
+          <div class="form-group">
+             <label for="module_route" class=" control-label col-md-4">Module Route</label>
+                <div class="col-md-8">
+                    <input  type='text' name='module_route' id='module_route' class="form-control " value='{{ isset($setting['module_route']) ? $setting['module_route'] : $module_name }}'  />		  
+                </div> 
+          </div> 
 		  <div class="form-group">
 			<label for="ipt" class=" control-label col-md-4"> Grid Table Type </label>
 			<div class="col-md-8">
