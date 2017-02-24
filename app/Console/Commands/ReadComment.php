c25b6f4d664a34deaf7260599569e6dd98daa84e
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-02-24 20:01:38 +0530

    [tickets] fix for read comments

--- app/Console/Commands/ReadComment.php
+++ app/Console/Commands/ReadComment.php
@@ -120,7 +120,7 @@ class ReadComment extends Command
                 
                 $L->log('Adding comment to database', $commentsData);
                 $id = $comment_model->insertRow($commentsData, NULL);
-                Servicerequests::where("TicketID", $ticketId)->update(['updated', $posted]);
+                Servicerequests::where("TicketID", $ticketId)->update(['updated' => $posted]);
                 
                 $L->log('Delete email');
                 imap_delete($inbox, $email_number);
