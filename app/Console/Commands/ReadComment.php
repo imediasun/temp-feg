74ffa778321b7944bd858ef4c52bcb3aa588cfad
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-03-01 05:52:54 +0530

    [tickets] [test] read ticket reply email from gmail with decoding if required

--- app/Console/Commands/ReadComment.php
+++ app/Console/Commands/ReadComment.php
@@ -181,12 +181,13 @@ class ReadComment extends Command
         return date("Y-m-d H:i:s", strtotime($meta->date));
     }
     public function getMessage($inbox, $email_number, $structure = ["1.1", "1"]) {
-        foreach($structure as $structureNumber) {
-            $message = imap_fetchbody($inbox, $email_number, $structureNumber);
-            if (!empty($message)) {
-                break;
-            }
-        }
+//        foreach($structure as $structureNumber) {
+//            $message = imap_fetchbody($inbox, $email_number, $structureNumber);
+//            if (!empty($message)) {
+//                break;
+//            }
+//        }
+        $message = $this->getMessageFromStructure($inbox, $email_number, $structure);        
         if (empty($message)) {
             $message = '';
         }
@@ -199,6 +200,7 @@ class ReadComment extends Command
         
         $message = preg_replace('/^[\r\n\t\s]+?/', '', $message);
         $message = preg_replace('/[\r\n\t\s]+?$/', '', $message);
+        $message = preg_replace('/\r\n{2,}|\r{2,}|\n{2,}/', "\n", $message);
         if (empty($message)) {
             $message = '';
         }
@@ -218,4 +220,112 @@ class ReadComment extends Command
             'department_id' => '',                
         ]);        
     }
+    
+    public function getMessageFromStructure($connection, $messageNumber, $partNumbers) {
+        $structure = imap_fetchstructure($connection, $messageNumber);
+        $flattenedParts = $this->emailFlattenParts($structure->parts);
+        $message = "";
+        foreach($flattenedParts as $partNumber => $part) {
+
+            switch($part->type) {
+
+                case 0:
+                    // the HTML or plain text part of the email
+                    $message = $this->emailGetPart($connection, $messageNumber, $partNumber, $part->encoding);
+                    // now do something with the message, e.g. render it
+                break;
+
+                case 1:
+                    // multi-part headers, can ignore
+
+                break;
+                case 2:
+                    // attached message headers, can ignore
+                break;
+
+                case 3: // application
+                case 4: // audio
+                case 5: // image
+                case 6: // video
+                case 7: // other
+                    //$filename = $this->emailgetEmailAttachmentFilenameFromPart($part);
+                    //if($filename) {
+                        // it's an attachment
+                        //$attachment = $this->emailGetPart($connection, $messageNumber, $partNumber, $part->encoding);
+                        // now do something with the attachment, e.g. save it somewhere
+                    //}
+                    //else {
+                        // don't know what it is
+                    //}
+                break;
+
+            }
+            
+            if (!empty($message) && in_array($partNumber, $partNumbers)) {
+                break;
+            }
+        }    
+        return $message;
+    }
+    
+    private function emailFlattenParts($messageParts, $flattenedParts = array(), $prefix = '', $index = 1, $fullPrefix = true) {
+
+        foreach($messageParts as $part) {
+            $flattenedParts[$prefix.$index] = $part;
+            if(isset($part->parts)) {
+                if($part->type == 2) {
+                    $flattenedParts = $this->emailFlattenParts($part->parts, $flattenedParts, $prefix.$index.'.', 0, false);
+                }
+                elseif($fullPrefix) {
+                    $flattenedParts = $this->emailFlattenParts($part->parts, $flattenedParts, $prefix.$index.'.');
+                }
+                else {
+                    $flattenedParts = $this->emailFlattenParts($part->parts, $flattenedParts, $prefix);
+                }
+                unset($flattenedParts[$prefix.$index]->parts);
+            }
+            $index++;
+        }
+
+        return $flattenedParts;
+			
+    }
+    
+    private function emailGetPart($connection, $messageNumber, $partNumber, $encoding) {
+
+        $data = imap_fetchbody($connection, $messageNumber, $partNumber);
+        switch($encoding) {
+            case 0: return $data; // 7BIT
+            case 1: return $data; // 8BIT
+            case 2: return $data; // BINARY
+            case 3: return base64_decode($data); // BASE64
+            case 4: return quoted_printable_decode($data); // QUOTED_PRINTABLE
+            case 5: return $data; // OTHER
+        }
+    }  
+    
+    private function emailgetEmailAttachmentFilenameFromPart($part) {
+
+        $filename = '';
+
+        if($part->ifdparameters) {
+            foreach($part->dparameters as $object) {
+                if(strtolower($object->attribute) == 'filename') {
+                    $filename = $object->value;
+                }
+            }
+        }
+
+        if(!$filename && $part->ifparameters) {
+            foreach($part->parameters as $object) {
+                if(strtolower($object->attribute) == 'name') {
+                    $filename = $object->value;
+                }
+            }
+        }
+
+        return $filename;
+
+    }    
+    
 }
