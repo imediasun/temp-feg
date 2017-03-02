2f2a49e518d6a90338b1d5ba89105a6cecb61e50
Awais <shayansolutions@gmail.com>
2017-03-02 18:36:54 +0500

    [public access checkbox in every module]

--- resources/views/sximo/module/config.blade.php
+++ resources/views/sximo/module/config.blade.php
@@ -236,7 +236,16 @@
                /> Yes
               </label>
            </div> 
-        </div>                
+        </div>
+		<div class="form-group">
+          <label for="usesimplesearch" class=" control-label col-md-4">public access</label>
+          <div class="col-md-8">
+              <label class="checkbox">
+              <input type="checkbox" value="true" name="usesimplesearch" id="usesimplesearch"
+
+              </label>
+           </div>
+        </div>
         
         <div class="form-group">
           <label for="simplesearchbuttonwidth" class=" control-label col-md-4">Simple Search Button width</label>
