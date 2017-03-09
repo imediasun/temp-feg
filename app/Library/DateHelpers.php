5a1177babe4cd39855efd7bbff0197177f6daf3e
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-03-10 04:37:09 +0530

    [date helper] custom formatter now added with custom format names "usdate", "usdatetime", "usdatetime_wos" etc.

--- app/Library/DateHelpers.php
+++ app/Library/DateHelpers.php
@@ -24,6 +24,17 @@ class DateHelpers{
 
     public static  function formatDateCustom($date, $format = "m/d/Y h:i:s A"){
         $formattedValue = '';
+        $customFormats = [
+            "usdate" => 'm/d/y',
+            "usdatetime" => 'm/d/Y h:i:s A',
+            "usdatetime_wos" => 'm/d/Y h:i A',
+            "usdtime" => 'h:i:s A',
+            "usdtime_wos" => 'h:i A',
+            ];
+        if (isset($customFormats[$format])) {
+            $format = $customFormats[$format];
+        }
+        
         if (preg_match('/[1-9]/',$date) && !is_null($date)){
             $formattedValue = date($format,strtotime(str_replace("/","-",$date)));
         }
