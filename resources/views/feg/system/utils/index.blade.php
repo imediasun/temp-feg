2961154b3af7d43531f26efbe4ee05faf1564a87
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-03-13 08:10:28 +0530

    [utils] Added FEG utitities controller. feg/system/utilities/modules/formatters shows all formatter functions applied to fields of all modules

---
+++ resources/views/feg/system/utils/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="page-content row" style="padding: 1% 2%; background-color: #fff;">    
+    <h2>FEG System Utilities Manager</h2>
+    <?php //var_dump(@$request); ?> 
+    <?php //var_dump(@$params); ?> 
+</div>	
+@endsection
---
+++ resources/views/feg/system/utils/modules/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="page-content row" style="padding: 1% 2%; background-color: #fff;">    
+    <h2>FEG System Utilities Manager &gt; Modules</h2>
+</div>	
+@endsection
