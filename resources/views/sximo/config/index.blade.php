724be94294a5be6010e6fd3fdc3f88da740fe31e
adnan <shayansolutions@gmail.com>
2017-03-17 04:59:44 +0500

    [fixing redemption gallery image upload and logo update in settings]

--- resources/views/sximo/config/index.blade.php
+++ resources/views/sximo/config/index.blade.php
@@ -145,10 +145,10 @@
 		    <label  class=" control-label col-md-4">Backend Logo</label>
 			<div class="col-md-8">
 				<input type="file" name="logo">
-				<p> <i>Please use image dimension 155px * 30px </i> </p>
+				<p> <i>Please use image dimension 155px * 30px </i> {{ $options['CNF_LOGO'] }}</p>
 				<div style="padding:5px; border:solid 1px #ddd; background:#f5f5f5; width:auto;">
-				 	@if(file_exists(public_path().'/sximo/images/'.$options['CNF_LOGO']) && $options['CNF_LOGO'] !='')
-				 	<img src="{{ asset('sximo/images/'.$options['CNF_LOGO'])}}" alt="{{ $options['CNF_APPNAME'] }}" />
+				 	@if(file_exists(public_path().'/sximo/images/'.CNF_LOGO) && CNF_LOGO !='')
+				 	<img src="{{ asset('sximo/images/'.CNF_LOGO)}}" alt="{{ $options['CNF_APPNAME'] }}" />
 				 	@else
 					<img src="{{ asset('sximo/images/logo.png')}}" alt="{{ $options['CNF_APPNAME'] }}" />
 					@endif	
