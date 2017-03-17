6e3bdb4e67f5c447d462d2e196e4a23bca7948cd
Arslan Ali <shayansolutions@gmail.com>
2017-03-17 08:52:31 +0500

    [Debug]empty reminder email

--- resources/views/user/emails/auth/reminder.blade.php
+++ resources/views/user/emails/auth/reminder.blade.php
@@ -1,1 +1,18 @@
-Hello world
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Password Reset</h2>
+
+		<div>
+            @if(isset($token))
+			To reset your password, complete this form: {{ URL::to('user/reset?token='.$token) }}.
+                @elseif(isset($id))
+            To reset your password, complete this form: {{ URL::to('user/reset?id='.$id) }}.
+                @else
+                @endif
+		</div>
+	</body>
+</html>
