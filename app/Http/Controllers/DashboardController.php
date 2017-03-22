c90a0446c13c59f75294b03e3b63df262fcdf4f6
MArslan Ali <shayansolutions@gmail.com>
2017-03-22 19:07:42 +0500

    small changing in redirection

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,12 +19,20 @@ class DashboardController extends Controller
 
     public function getIndex(Request $request)
     {
+        if(Auth::user() && Auth::user()->redirect_link)
+        {
+            return redirect(Auth::user()->redirect_link);
+        }
         $group = Auth::user()?Auth::user()->group_id:0;
         $redirect = Groups::find($group)?Groups::find($group)->redirect_link:'';
         if (!empty($redirect)) {
             return redirect($redirect);
         }
-        
+        require_once('setting.php');
+        if(CNF_REDIRECTLINK)
+        {
+            return redirect(CNF_REDIRECTLINK);
+        }
         /* connect to gmail */
         $this->data['online_users'] = \DB::table('users')->orderBy('last_activity', 'desc')->limit(10)->get();
         return view('dashboard.index', $this->data);
