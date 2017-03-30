2be303c551b79908cd285bc61708b31abe03aec2
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-03-30 22:27:33 +0530

    [ticket list] Fix last updated count

--- app/Models/Servicerequests.php
+++ app/Models/Servicerequests.php
@@ -19,27 +19,25 @@ class Servicerequests extends Observerable  {
 
     public static function querySelect(  ){
         $date = date("Y-m-d");
-        $sql = "SELECT concat(U.first_name, ' ', U.last_name) as last_user, sb_tickets.*
-                FROM (
-                    SELECT
-                        c.last_user_id,
-                        IF (
-                            ISNULL(c.Posted),
-                                DATEDIFF('$date', Created),
-                                DATEDIFF('$date', c.Posted)
-                            ) as last_updated_elapsed_days,
-                        sb_tickets.*
-                    FROM sb_tickets
-                    LEFT JOIN (SELECT
-                                    TicketID,
-                                    Posted,
-                                    UserID AS last_user_id
-                                FROM sb_ticketcomments
-                                ORDER BY Posted DESC
-                        ) c ON c.TicketID = sb_tickets.TicketID
-                    GROUP BY sb_tickets.TicketID) sb_tickets
-                LEFT JOIN users U ON U.id = sb_tickets.last_user_id
-            ";
+        $sql = "SELECT
+            
+                    CONCAT(U.first_name, ' ', U.last_name) AS last_user,
+                    IF(ISNULL(sbc.Posted),
+                        DATEDIFF('$date', sb_tickets.Created),
+                        DATEDIFF('$date', sbc.Posted)) AS last_updated_elapsed_days,
+                    sb_tickets.*
+
+                FROM sb_tickets
+
+        LEFT JOIN (
+            SELECT sb_ticketcomments.TicketID, Posted, UserID
+				FROM sb_ticketcomments
+				LEFT JOIN (SELECT tc.TicketID, max(tc.Posted) as max_posted from sb_ticketcomments tc group by TicketID) tcm
+					ON tcm.TicketID = sb_ticketcomments.TicketID
+				WHERE tcm.max_posted = sb_ticketcomments.Posted
+    ) sbc ON sbc.TicketID = sb_tickets.TicketID
+
+	LEFT JOIN users U ON U.id = sbc.UserID";
 
         return $sql;
     }
