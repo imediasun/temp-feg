3d4a2417ea43bd9ae4303803378f78632f02b9e8
adnan <shayansolutions@gmail.com>
2017-04-13 16:47:58 +0500

    [making date orderd readonly in order form]

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -139,10 +139,10 @@
                             Date Ordered</label>
 
                         <div class="col-md-8" style="padding-left: 13px;">
-                            <div class="input-group datepicker" style="width:150px !important; margin-bottom: 9px;">
+                            <div class="input-group " style="width:150px !important; margin-bottom: 9px;">
                                 <input type="text" class="form-control " id="my-datepicker" name="date_ordered"
-                                       value="{{ date("m/d/Y", strtotime($data['today'])) }}" required="required"/>
-                                <span class="input-group-addon "><i class="fa fa-calendar" id="icon"></i></span>
+                                       value="{{ date("m/d/Y", strtotime($data['today'])) }}" required="required" readonly/>
+                                <span class="input-group-addon " ><i class="fa fa-calendar" id="icon"></i></span>
                             </div>
                         </div>
 
@@ -392,11 +392,11 @@
 
             $('.test').val(0.00);
 
-            $('#icon').click(function () {
+           /* $('#icon').click(function () {
                 $(document).ready(function () {
                     $("#my-datepicker").datepicker().focus();
                 });
-            });
+            });*/
             $("#location_id").jCombo("{{ URL::to('order/comboselect?filter=location:id:id|location_name ') }}",
                     {
                         selected_value: "{{ $data["order_loc_id"]}}",
