caf61a8bd1824d38120a55e8dc68858e99544f0b
aqeel <intern2@shayansolutions.com>
2017-04-21 20:42:02 +0500

    inserted die

--- app/Http/Controllers/ManagefreightquotersController.php
+++ app/Http/Controllers/ManagefreightquotersController.php
@@ -415,7 +415,7 @@ class ManagefreightquotersController extends Controller
             }
             if(!empty($to_city))
             {
-                $subject  .= is_array($to_city)?(implode(',',$to_city) == ''|| implode(',',$to_city)) == ' ' ?'' :' To '.implode(',',$to_city):($to_city != ' ' || $to_city != '')?' To '.$to_city:'';
+                $subject  .= is_array($to_city)?(implode(',',$to_city) == ''|| implode(',',$to_city) == ' ') ?'' :' To '.implode(',',$to_city):($to_city != ' ' || $to_city != '')?' To '.$to_city:'';
                 dd($subject,$to_city,implode(',',$to_city));
             }
             $subject .= '';
