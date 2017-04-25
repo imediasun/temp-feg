318a258875aa1ae71d10332cb89becf2654f2559
aqeel <intern2@shayansolutions.com>
2017-04-25 14:46:42 +0500

    testing

--- resources/views/core/users/table.blade.php
+++ resources/views/core/users/table.blade.php
@@ -106,7 +106,7 @@
 			  </tr>
 			  @endif
 
-              @foreach ($rowData as $row){{dd($row)}}
+              @foreach ($rowData as $row)
            		<?php $id = $row->id;?>
                 <tr class="editable" id="form-{{ $row->id }}" @if($setting['inline']!='false' && $setting['disablerowactions']=='false') data-id="{{ $row->id }}" ondblclick="showFloatingCancelSave(this)" @endif>
 					@if(!isset($setting['hiderowcountcolumn']) || $setting['hiderowcountcolumn'] != 'true')
@@ -131,7 +131,7 @@
                                <img alt="" src="{{url()}}/silouette.png" width="30" height="30" class="img-circle test" border="0"/>
                                <?php } ?>
                            @elseif($field['field'] =='active')
-                                <input type='checkbox' name="mycheckbox" {{($value == "Yes") ?  'checked':'' }}	data-size="mini" data-animate="true"
+                                <input type='checkbox' name="mycheckbox" {{($row->active == 1) ?  'checked':'' }}	data-size="mini" data-animate="true"
                                        data-on-text="Active" data-off-text="Inactive" data-handle-width="50px" class="toggle" data-id="{{$row->id}}"
                                        id="toggle_trigger_{{$row->id}}" onSwitchChange="trigger()" />
                             @elseif($field['field'] =='date')
