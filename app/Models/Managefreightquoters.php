874d7dfb3161af7ebc167c81b0be9284d0ea3aff
aqeel <intern2@shayansolutions.com>
2017-04-25 19:39:01 +0500

    emails will go to users not to stanly in freight tools

--- app/Models/Managefreightquoters.php
+++ app/Models/Managefreightquoters.php
@@ -491,11 +491,11 @@ class managefreightquoters extends Sximo
 
                         $from = \Session::get('eid');
                         $to = $this->get_user_emails('users_plus_district_and_field_managers', $data['request']['loc'][$i]);
-                        $to = "stanlymarian@gmail.com";//hardcoded email for testing
-                        //$cc = 'freight-notifications@fegllc.com';
-                        $cc = 'jdanial710@gmail.com';
-                        //$bcc = 'support@fegllc.com';
-                        $bcc = 'daynaedvin@gmail.com';
+                        //$to = "stanlymarian@gmail.com";//hardcoded email for testing
+                        $cc = 'freight-notifications@fegllc.com';
+                        //$cc = 'jdanial710@gmail.com';
+                        $bcc = 'support@fegllc.com';
+                        //$bcc = 'daynaedvin@gmail.com';
                         $subject = '('.(int)$num_games_per_destination.')'.' Game[s] scheduled for delivery to ' . $locationName . '!';
                         $message = '<p>
 										' . $email_notes . '
