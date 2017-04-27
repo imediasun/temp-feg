3333ce1ae7fe394de01bea837d37b753274cfa7f
aqeel <intern2@shayansolutions.com>
2017-04-27 20:06:04 +0500

    dieing in code

--- app/Library/FEG/System/FEGSystemHelper.php
+++ app/Library/FEG/System/FEGSystemHelper.php
@@ -444,10 +444,10 @@ class FEGSystemHelper
     }
     public static function laravelMail($to, $subject, $message, $from = "support@fegllc.com", $options = array()) {
         $view = empty($options['view']) ? '': $options['view'];
-//        $options['to'] = $to;
-//        $options['subject'] = $subject;
-//        $options['message'] = $message;
-//        $options['from'] = $from;
+        $options['to'] = $to;
+        $options['subject'] = $subject;
+        $options['message'] = $message;
+        $options['from'] = $from;
 
         if (!empty($view)) {
             Mail::send($view, $options, function ($mail) use ($options) {
