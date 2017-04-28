b3057ed9394dd0e0447dce725cdcc256ba007eae
aqeel <intern2@shayansolutions.com>
2017-04-28 19:38:47 +0500

    Google redirect issue solved

--- config/services.php
+++ config/services.php
@@ -40,7 +40,7 @@ return [
 	    //'client_secret' => env('G_SECRET'),
         'client_id' => env('G_ID'),
         'client_secret' => env('G_SECRET'),
-	    'redirect' => $_SERVER['SERVER_NAME'].env('G_REDIRECT'),
+	    'redirect' => 'http://'.$_SERVER['SERVER_NAME'].env('G_REDIRECT'),
 	],
 
 	'twitter' => [
