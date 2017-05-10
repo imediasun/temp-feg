38da40345adb8bf9383cb3acfac9769849ccc9d1
Asad Mahmood <intern2@shayansolutions.com>
2017-05-10 17:46:46 +0500

    added code to check access

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,7 @@ use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Auth;
+use App\Models\Sximo\Module;
 
 class DashboardController extends Controller
 {
@@ -24,8 +25,7 @@ class DashboardController extends Controller
     {
         if(Auth::user() && Auth::user()->redirect_link)
         {
-            $pageOrModule = explode('/',Auth::user()->redirect_link);
-            $pageOrModule = $pageOrModule[count($pageOrModule)-1];
+
             /*$client = new Client();
             $res = $client->request('GET', url().'/core/users/check-access?module='.$pageOrModule);
             $result = $res->getBody();
@@ -41,13 +41,19 @@ class DashboardController extends Controller
                 dd($response->getBody());
             });
             $promise->wait();*/
-            return redirect(Auth::user()->redirect_link == "dashboard"?'user/profile':Auth::user()->redirect_link);
+            if(Auth::user()->redirect_link != "dashboard" && self::accessCheck(Auth::user()->redirect_link))
+            {
+                return redirect(Auth::user()->redirect_link);
+            }
+            else{
+                return redirect('user/profile');
+            }
+
         }
         $group = Auth::user()?Auth::user()->group_id:0;
         $redirect = Groups::find($group)?Groups::find($group)->redirect_link:'';
-
-        if (!empty($redirect) && $redirect != "dashboard") {
-
+        if(!empty($redirect) && $redirect != "dashboard" && self::accessCheck($redirect))
+        {
             return redirect($redirect);
         }
         //require_once('setting.php');
@@ -57,11 +63,57 @@ class DashboardController extends Controller
         }*/
 
         $redirect = Options::where('option_name','CNF_REDIRECLINK')->pluck('option_value');
+        if(!empty($redirect) && $redirect != "dashboard" && self::accessCheck($redirect))
+        {
+            return redirect($redirect);
+        }
+        else{
+            return redirect('user/profile');
+        }
 
-        return redirect($redirect == "dashboard"?'user/profile': $redirect);
         /* connect to gmail */
         $this->data['online_users'] = \DB::table('users')->orderBy('last_activity', 'desc')->limit(10)->get();
         return view('dashboard.index', $this->data);
+    }
+
+    public static function accessCheck($redirect)
+    {
+        $pageOrModule = explode('/',Auth::user()->redirect_link);
+        $pageOrModule = $pageOrModule[count($pageOrModule)-1];
+        $moduleId = Module::where('module_name', $pageOrModule)->pluck('module_id');
+        if (!empty($moduleId)) {
+            $row = \DB::table('tb_groups_access')->where('module_id', '=', $moduleId)
+                ->where('group_id', '=', \Session::get('gid'))
+                ->get();
+
+            if (count($row) >= 1) {
+                $row = $row[0];
+                if ($row->access_data != '') {
+                    $data = json_decode($row->access_data, true);
+                } else {
+                    $data = array();
+                }
+                return $data;
+            } else {
+                return false;
+            }
+        }
+        else {
+            $row = \DB::table('tb_pages')->where('alias', '=', $pageOrModule)
+                ->first();
+
+            if (!empty($row)) {
+                $data = ['is_view' => 0];
+                if ($row->access != '') {
+                    $accsss = json_decode($row->access, true);
+                    $data['is_view'] = isset($accsss[\Session::get('gid')])? $accsss[\Session::get('gid')] : 0;
+                }
+                return $data;
+            }
+            else {
+                return false;
+            }
+        }
     }
 
 }
