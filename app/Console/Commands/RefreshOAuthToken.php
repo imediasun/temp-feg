ba69dd25d5d69a4076fe83cd90acd1136182852f
Asad Mahmood <intern2@shayansolutions.com>
2017-05-12 16:16:05 +0500

    cron job for refresh token updated added logs and handling users with same oauth emails

--- app/Console/Commands/RefreshOAuthToken.php
+++ app/Console/Commands/RefreshOAuthToken.php
@@ -6,7 +6,7 @@ use App\Models\Core\Users;
 use GuzzleHttp\Exception\ClientException;
 use Illuminate\Console\Command;
 use GuzzleHttp\Client;
-use League\OAuth2\Client\Provider\Google;
+use App\Library\FEG\System\FEGSystemHelper;
 
 class RefreshOAuthToken extends Command
 {
@@ -17,6 +17,7 @@ class RefreshOAuthToken extends Command
      */
     protected $signature = 'refresh:token';
 
+    protected $L = null;
     /**
      * The console command description.
      *
@@ -44,12 +45,16 @@ class RefreshOAuthToken extends Command
         if (env('DONT_REFRESH_OAUTH_TOKEN', false) === true) {
             return;
         }
+        global $__logger;
+        $L = $this->L = $__logger = FEGSystemHelper::setLogger($this->L, "refresh-oauth-token.log", "FEGOAuthTokenCron/RefreshOAuthToken", "REFRESH_OAUTH");
+        $L->log('Start Refreshing Oauth Tokens');
 
-        $users = Users::whereNotNull('refresh_token')->get();
+        $users = Users::whereNotNull('refresh_token')->orWhere('refresh_token','!=','')->get();
+        $count = count($users);
+        $L->log($count.' Users with Oauth Refresh Token Found');
+        $client = new Client();
         foreach ($users as $key=>$user){
             //echo $user->refresh_token;
-            $count = count($users);
-            $client = new Client();
             try{
                 $res = $client->request('POST', 'https://www.googleapis.com/oauth2/v4/token',array('headers'=>array('Content-Type'=>'application/x-www-form-urlencoded'),'form_params'=>array(
                     'grant_type'=>'refresh_token',
@@ -60,13 +65,25 @@ class RefreshOAuthToken extends Command
                     'client_secret'=>env('G_SECRET'))));
                 $result = $res->getBody();
                 $array = json_decode($result, true);
-
+                $users_with_related_oauthemail = Users::where('oauth_email',$user->oauth_email)->get();
+                $L->log(count($users_with_related_oauthemail).' Users with same oauth email Found');
+                foreach ($users_with_related_oauthemail as $related)
+                {
+                    $related->oauth_token = $array['access_token'];
+                    if(isset($array['refresh_token'])){
+                        $related->refresh_token = $array['refresh_token'];
+                    }
+                    $related->save();
+                    $L->log('ID '.$related->id.' User (with same oauth email) oauth token updated');
+                }
                 $user->oauth_token = $array['access_token'];
                 if(isset($array['refresh_token'])){
                     $user->refresh_token = $array['refresh_token'];
                 }
 
                 $user->save();
+                $L->log('ID '.$user->id.' User oauth token updated');
+                $L->log('Google Api (Refresh token) response for user id '.$user->id.' '.json_encode($array));
                 print_r($array);
             }
             catch (ClientException $e)
@@ -77,10 +94,12 @@ class RefreshOAuthToken extends Command
                     $user->save();
                     echo 'User ID '.$user->id . ' token could not be updated..';
                     $count--;
-
+                $L->log('Google Api (Refresh token) error response for user id '.$user->id.' '.$e);
             }
 
         }
+        $L->log($count .' users of '. count($users). ' Users refresh token updated');
+        $L->log('Cron job refresh token End');
         echo $count .' users of '. count($users). ' Users refresh token updated';
         return true;
     }
