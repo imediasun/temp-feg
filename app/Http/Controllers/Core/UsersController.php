b580eb8383c351a15078d9fcbfeea329f4838858
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-05-12 07:14:57 +0530

    [user][session][impersonation] Include missing user_location_ids in session

--- app/Http/Controllers/Core/UsersController.php
+++ app/Http/Controllers/Core/UsersController.php
@@ -200,10 +200,15 @@ class UsersController extends Controller
         Session::put('company_id', $row->company_id);
         $user_locations = \SiteHelpers::getLocationDetails($row->id);
         if (!empty($user_locations)) {
-            Session::put('user_locations', $user_locations);
-            Session::put('selected_location', $user_locations[0]->id);
-            Session::put('selected_location_name', $user_locations[0]->location_name_short);
+            $user_locations = [];
         }
+        $user_location_ids = \SiteHelpers::getIdsFromLocationDetails($user_locations);
+        $has_all_locations = empty($row->has_all_locations) ? 0 : 1;
+        \Session::put('user_has_all_locations', $has_all_locations); 
+        Session::put('user_locations', $user_locations);
+        Session::put('selected_location', @$user_locations[0]->id);
+        Session::put('selected_location_name', @$user_locations[0]->location_name_short);
+        \Session::put('user_location_ids', $user_location_ids);
         Session::put('get_locations_by_region', $row->get_locations_by_region);
         Session::put('email_2', $row->email_2);
         Session::put('primary_phone', $row->primary_phone);
