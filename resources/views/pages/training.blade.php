6bfddb02efd9b6ef40aa596234b63d4fe541d2e8
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-05-15 05:19:16 +0530

    [pages] downloaded from server and pushed to repo + change in [benefits]  as per last commit

--- resources/views/pages/training.blade.php
+++ resources/views/pages/training.blade.php
@@ -1,20 +1,20 @@
-@extends('layouts.app') @section('content')
-    <div class="page-content-wrapper m-t">
-        <div class="sbox animated fadeInRight">
-            <div class="sbox-title"> {{ $pageTitle }}{!! $editLink !!}</div>
-            <div class="sbox-content">
-                <div class="col-md-12" style="padding-top: 50px; padding-right: 50px; padding-bottom: 50px; background-color: #ffffff;">
-
-                <p><strong>Game Down Problem & Solution Documentation </strong><span style="color: #808080;">by Rich Pankey</span></p>
-<p><iframe src="//www.youtube.com/embed/vXVAmz-GZ9g" width="560" height="314" allowfullscreen="allowfullscreen"></iframe></p>
-<p><strong>Installing an Eye Bolt </strong><span style="color: #808080;">by Tom Hansen</span></p>
-<p><iframe src="//www.youtube.com/embed/YpLn81RE_Pw" width="560" height="314" allowfullscreen="allowfullscreen"></iframe></p>
-                </div>
-                <div class="clearfix"> </div>
-            </div>
-        </div>
-        <div class="clearfix"> </div>
-
-    </div>
-    <div class="clearfix"> </div>
-@stop
+@extends('layouts.app') @section('content')
+    <div class="page-content-wrapper m-t">
+        <div class="sbox animated fadeInRight">
+            <div class="sbox-title"> {{ $pageTitle }}{!! $editLink !!}</div>
+            <div class="sbox-content">
+                <div class="col-md-12" style="padding-top: 50px; padding-right: 50px; padding-bottom: 50px; background-color: #ffffff;">
+
+                <p><strong>Game Down Problem & Solution Documentation </strong><span style="color: #808080;">by Rich Pankey</span></p>
+<p><iframe src="//www.youtube.com/embed/vXVAmz-GZ9g" allowfullscreen="allowfullscreen" width="560" height="314"></iframe></p>
+<p><strong>Installing an Eye Bolt </strong><span style="color: #808080;">by Tom Hansen</span></p>
+<p><iframe src="//www.youtube.com/embed/YpLn81RE_Pw" allowfullscreen="allowfullscreen" width="560" height="314"></iframe></p>
+                </div>
+                <div class="clearfix"> </div>
+            </div>
+        </div>
+        <div class="clearfix"> </div>
+
+    </div>
+    <div class="clearfix"> </div>
+@stop
