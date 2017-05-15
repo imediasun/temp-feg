4496efac57b2e4f0c73f1fb1f881d58b581b7f41
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-05-15 08:05:23 +0530

    [forum] [jsconnect] SSO response [fix]

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -394,7 +394,7 @@ class UserController extends Controller
         return view('user.profile', $this->data);
     }
 
-    public function jsconnect(Request $request) {
+    public function getJsconnect(Request $request) {
 
         $inputs = $request->all();
 
