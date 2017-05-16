a4e844865643a575bfbdf55301bc9d0b2d5413f7
Asad Mahmood <intern2@shayansolutions.com>
2017-05-16 12:33:37 +0500

    jsconnect issue resolved updated

--- app/Http/Controllers/FreightquotersController.php
+++ app/Http/Controllers/FreightquotersController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use \App\Models\Sximo\Module;
 use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 use Validator, Input, Redirect;
-use App\Models\Sximo\Module;
 
 
 class FreightquotersController extends Controller
