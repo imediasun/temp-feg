842be77c60e1ae41df3a42cd452361529b00c2b6
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-05-17 14:16:45 +0530

    [test] example showing how a function of a controller can be executed from another code or place

--- app/Http/Controllers/Feg/System/Utils/ModulesController.php
+++ app/Http/Controllers/Feg/System/Utils/ModulesController.php
@@ -28,6 +28,33 @@ class ModulesController extends Controller
         $this->data['data'] = [$params];
 		return view('feg.system.utils.modules.index',$this->data);
 	}
+	public function loadlocations($params = null)
+	{
+        $locationControllerInstance = new \App\Http\Controllers\LocationController;
+        $request = new \Illuminate\Http\Request;
+        var_dump("
+
+            Example showing how to run a controller function (that does not return a view) from a controller instance.
+
+            \$locationControllerInstance = new \App\Http\Controllers\LocationController;
+            \$locationControllerInstance->getSearchFilterQuery(\"id:like:2007|active:equal:1|\")
+
+            Result returned: \"".$locationControllerInstance->getSearchFilterQuery("id:like:2007|active:equal:1|")."\"
+
+                ");
+
+
+        var_dump("
+
+            Example showing how to run a function that returns a view from a controller instance (view showin below)
+
+            \$locationControllerInstance = new \App\Http\Controllers\LocationController;
+            \$request = new \Illuminate\Http\Request;
+            \$locationControllerInstance->getIndex(\$request);
+
+            ");
+        return $locationControllerInstance->getIndex($request);
+	}
 
 	public function formatters($params = null)
 	{
