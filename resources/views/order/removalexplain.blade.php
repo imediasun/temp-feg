8b593191214daec8eba6d8172912c46cb2505c1a
Haider Ali <dev1@shayansolutions.com>
2017-05-17 13:13:04 +0500

    406 fixed revised.

--- resources/views/order/removalexplain.blade.php
+++ resources/views/order/removalexplain.blade.php
@@ -15,20 +15,21 @@
                 <div class="sbox-title"> <h4> <i class="fa fa-table"></i> Request Removal <small>Order Removal Request</small></h4></div>
                 <div class="sbox-content">
                     <div class="row" >
-                        <div class="col-md-12" style="">
+                        <div class="col-md-8" style="">
                             <h1>Request Removal Explain</h1>
                         </div>
 
-                        <div class="col-md-12" style="margin-left:16px;">
+                        <div class="col-md-6" style="margin-left:16px;">
                             {!! Form::open(array('url'=>'order/removalrequest', 'class'=>'form-horizontal','files' => true , 'parsley-validate'=>'','novalidate'=>' ','id'=>'removalrequestform')) !!}
-                            <label class="label-control" for="explaination">Please Explain Why?</label>
                             <div class="form-group">
                                 <textarea rows="8" cols="80" 
                                           name="explaination" id="explaination"
-                                          required></textarea>
+                                          placeholder="Please Explain Why?"
+                                          required>
+                                </textarea>
                                 <input type="hidden" value="{{$po_number }}" name="po_number"/>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group" style="text-align: center;">
                                 <button type="submit" name="submit" class="btn btn-primary btn-sm"><i class="fa  fa-save "></i>  Send Request </button>
                                 <button type="button" onclick="location.href='{{ URL::to('order?return='.$return) }}' " class="btn btn-success btn-sm "><i class="fa  fa-arrow-circle-left "></i>  {{ Lang::get('core.sb_cancel') }} </button>
 
