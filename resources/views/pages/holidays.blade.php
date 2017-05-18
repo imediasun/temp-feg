55d3c34a4156e3b387474cb56ad2ea72d55932a0
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-05-19 01:59:51 +0530

    [page] holidays and benefits - updated 2017-18 Benefit Enrollment form

--- resources/views/pages/holidays.blade.php
+++ resources/views/pages/holidays.blade.php
@@ -1,125 +1,1 @@
-@extends('layouts.app') @section('content')
-<div class="page-content-wrapper m-t">
-    <div class="sbox animated fadeInRight" style="">
-        <div class="sbox-title"> Benefits / {{ $pageTitle }}</div>
-        <div class="sbox-content">
-            <div class="col-md-12 " style="padding-top: 50px;  padding-bottom: 50px;  background-color: rgb(255, 255, 255);">
-                <div>
-                    <span style="color: rgb(128, 128, 128);">Please contact HR at 
-                        <a href="mailto:hr@fegllc.com" target="_blank" style="color: rgb(128, 128, 128);">hr@fegllc.com</a>
-                         with any questions.
-                    </span>
-                </div>
-                <div>
-                    <span style="color: rgb(128, 128, 128);">Click on links below to download documents..</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <a class="download" title="**NEW** 2015-16 Benefit Enrollment Package" href="/benefits/14E_Benefit%20Enrollment%20Package-2015-2016.pdf" target="_blank">
-                        <span style="color: rgb(255, 0, 0);"><strong>**NEW**</strong> </span>
-                        <span style="color: rgb(128, 128, 128);">2017-18 Benefit Enrollment Package</span>
-                    </a>
-                </div>
-                <div style="padding-left: 60px;">
-                    <a title="FEG 401K Plan Highlights" href="/benefits/Plan%20Highlights%202015-2016.pdf" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">FEG 401K Plan Highlights</span>
-                    </a>
-                </div>
-                <div style="padding-left: 60px;">
-                    <a title="FEG 401K Plan Eligibility Guide" href="/benefits/Eligibility%20Guide%202015-2016.pdf" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">FEG 401K Plan Eligibility Guide</span>
-                    </a>
-                </div>
-                <div style="padding-left: 60px;">
-                    <a title="FEG401K Designation of Beneficiary Form" href="/benefits/Beneficiary%20Designation.pdf" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">FEG401K Designation of Beneficiary Form</span>
-                    </a>
-                </div>
-                <div style="padding-left: 60px;">
-                    <a title="Supplemental Benefits" href="/benefits/Marsh_supplemental_benefits.pdf" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">Supplemental Benefits</span>
-                    </a>
-                </div>
-                <div style="padding-left: 60px;">
-                    <a title="Supplemental Benefits - Additional Info" href="/benefits/supplemental_benefits_info.pdf" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">Supplemental Benefits - Additional Info</span>
-                    </a>                    
-                </div>
-                <div style="padding-left: 60px;">                    
-                    <a title="Locate a Doctor" href="/adp/UHC_Medical_Doc_Find.doc" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">Locate a Doctor</span>
-                    </a>                    
-                </div>
-                <div style="padding-left: 60px;">                    
-                    <a title="Locate a Dentist" href="/adp/Guardian_Dental_11-09.doc" target="_blank">
-                        <span style="color: rgb(128, 128, 128);">Locate a Dentist</span>
-                    </a>                    
-                </div>
-                <div style="padding-left: 60px;">                    
-                        <a title="Locate a Vision Specialist" href="/adp/VSP_Doc_Find_Non_Members.doc" target="_blank">
-                            <span style="color: rgb(128, 128, 128);">Locate a Vision Specialist</span>
-                        </a>                    
-                </div>
-                <div>
-                    <hr>
-                </div>
-                <div>
-                    <strong>Full-time employees will be paid for the following holidays:</strong>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">New Year's Day</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">Memorial Day</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">Independence Day (4th of July)</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">Labor Day</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">Thanksgiving Day</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">Day after Thanksgiving</span>
-                </div>
-                <div style="padding-left: 60px;">
-                    <span style="color: rgb(128, 128, 128);">Christmas Day</span>
-                </div>
-                <p><strong>Note: </strong>
-                    If a holiday falls on a Saturday, the time off will be granted on Friday; 
-                    if a holiday falls on a Sunday, the time off will be granted on Monday
-                    . 
-                    <br><br>
-                    Full time salaried employees who are required to work on a holiday 
-                    should receive a substitute day off at another time (this does
-                    not have to be the same payroll cycle). <br><br>Full time 
-                    hourly employees will get compensated at their straight pay rate 
-                    for holidays. You may be required to work on holidays and 
-                    you will get paid at your regular pay rate for time worked 
-                    in addition to the holiday pay. If you are required to work 
-                    on a holiday, you may be able to receive an alternate day 
-                    off during the same payroll cycle if approved by your manager. 
-                    This may not be possible as holidays generally occur during our 
-                    busy seasons. If you are unable to take a day off, 
-                    you will be receiving the additional 8 hours at a straight 
-                    pay rate for the unused holiday.</p><p><strong>Part time 
-                        hourly employees will receive wages at one and one half 
-                        times their straight time rate for hours worked on the 
-                        following holidays:</strong>
-                </p>
-                <div style="padding-left: 60px;">New Year's Day</div>
-                <div style="padding-left: 60px;">Memorial Day</div>
-                <div style="padding-left: 60px;">Independence Day (4th of July)</div>
-                <div style="padding-left: 60px;">Labor Day</div>
-                <div style="padding-left: 60px;">Thanksgiving Day</div>
-                <div style="padding-left: 60px;">Christmas Day</div>
-                    
-            </div>
-            <div class="clearfix"> </div>
-        </div>
-    </div>
-    <div class="clearfix"> </div>        
-</div>
-<div class="clearfix"> </div>
-@stop
+@extends('layouts.app') @section('content')<div class="page-content-wrapper m-t">    <div class="sbox animated fadeInRight" style="">        <div class="sbox-title"> Benefits / {{ $pageTitle }}{!! $editLink !!}</div>        <div class="sbox-content">            <div class="col-md-12 " style="padding-top: 50px;  padding-bottom: 50px;  background-color: rgb(255, 255, 255);">                <div>                    <span style="color: rgb(128, 128, 128);">Please contact HR at                         <a href="mailto:hr@fegllc.com" target="_blank" style="color: rgb(128, 128, 128);">hr@fegllc.com</a>                         with any questions.                    </span>                </div>                <div>                    <span style="color: rgb(128, 128, 128);">Click on links below to download documents..</span>                </div>                <div style="padding-left: 60px;">                    <a class="download" title="**NEW** 2015-16 Benefit Enrollment Package" href="/benefits/2017%20Employee%20Benefit%20Enrollment%20Kit%20A.pdf" target="_blank">                        <span style="color: rgb(255, 0, 0);"><strong>**NEW**</strong> </span>                        <span style="color: rgb(128, 128, 128);">2017-18 Benefit Enrollment Package</span>                    </a>                </div>                <div style="padding-left: 60px;">                    <a title="FEG 401K Plan Highlights" href="/benefits/Plan%20Highlights%202015-2016.pdf" target="_blank">                        <span style="color: rgb(128, 128, 128);">FEG 401K Plan Highlights</span>                    </a>                </div>                <div style="padding-left: 60px;">                    <a title="FEG 401K Plan Eligibility Guide" href="/benefits/Eligibility%20Guide%202015-2016.pdf" target="_blank">                        <span style="color: rgb(128, 128, 128);">FEG 401K Plan Eligibility Guide</span>                    </a>                </div>                <div style="padding-left: 60px;">                    <a title="FEG401K Designation of Beneficiary Form" href="/benefits/Beneficiary%20Designation.pdf" target="_blank">                        <span style="color: rgb(128, 128, 128);">FEG401K Designation of Beneficiary Form</span>                    </a>                </div>                <div style="padding-left: 60px;">                    <a title="Supplemental Benefits" href="/benefits/Marsh_supplemental_benefits.pdf" target="_blank">                        <span style="color: rgb(128, 128, 128);">Supplemental Benefits</span>                    </a>                </div>                <div style="padding-left: 60px;">                    <a title="Supplemental Benefits - Additional Info" href="/benefits/supplemental_benefits_info.pdf" target="_blank">                        <span style="color: rgb(128, 128, 128);">Supplemental Benefits - Additional Info</span>                    </a>                                    </div>                <div style="padding-left: 60px;">                                        <a title="Locate a Doctor" href="/adp/UHC_Medical_Doc_Find.doc" target="_blank">                        <span style="color: rgb(128, 128, 128);">Locate a Doctor</span>                    </a>                                    </div>                <div style="padding-left: 60px;">                                        <a title="Locate a Dentist" href="/adp/Guardian_Dental_11-09.doc" target="_blank">                        <span style="color: rgb(128, 128, 128);">Locate a Dentist</span>                    </a>                                    </div>                <div style="padding-left: 60px;">                                            <a title="Locate a Vision Specialist" href="/adp/VSP_Doc_Find_Non_Members.doc" target="_blank">                            <span style="color: rgb(128, 128, 128);">Locate a Vision Specialist</span>                        </a>                                    </div>                <div>                    <hr>                </div>                <div>                    <strong>Full-time employees will be paid for the following holidays:</strong>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">New Year's Day</span>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">Memorial Day</span>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">Independence Day (4th of July)</span>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">Labor Day</span>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">Thanksgiving Day</span>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">Day after Thanksgiving</span>                </div>                <div style="padding-left: 60px;">                    <span style="color: rgb(128, 128, 128);">Christmas Day</span>                </div>                <p><strong>Note: </strong>                    If a holiday falls on a Saturday, the time off will be granted on Friday;                     if a holiday falls on a Sunday, the time off will be granted on Monday                    .                     <br><br>                    Full time salaried employees who are required to work on a holiday                     should receive a substitute day off at another time (this does                    not have to be the same payroll cycle). <br><br>Full time                     hourly employees will get compensated at their straight pay rate                     for holidays. You may be required to work on holidays and                     you will get paid at your regular pay rate for time worked                     in addition to the holiday pay. If you are required to work                     on a holiday, you may be able to receive an alternate day                     off during the same payroll cycle if approved by your manager.                     This may not be possible as holidays generally occur during our                     busy seasons. If you are unable to take a day off,                     you will be receiving the additional 8 hours at a straight                     pay rate for the unused holiday.</p><p><strong>Part time                         hourly employees will receive wages at one and one half                         times their straight time rate for hours worked on the                         following holidays:</strong>                </p>                <div style="padding-left: 60px;">New Year's Day</div>                <div style="padding-left: 60px;">Memorial Day</div>                <div style="padding-left: 60px;">Independence Day (4th of July)</div>                <div style="padding-left: 60px;">Labor Day</div>                <div style="padding-left: 60px;">Thanksgiving Day</div>                <div style="padding-left: 60px;">Christmas Day</div>                                </div>            <div class="clearfix"> </div>        </div>    </div>    <div class="clearfix"> </div>        </div><div class="clearfix"> </div>@stop
