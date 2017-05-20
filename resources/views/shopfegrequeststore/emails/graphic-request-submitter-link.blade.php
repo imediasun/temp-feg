986a142872f53202e5ea4c9528557b7d9a6cd6c0
Arslan Ali <shayansolutions@gmail.com>
2017-05-20 15:44:02 +0500

    fixing minor issues in email content

--- resources/views/shopfegrequeststore/emails/graphic-request-submitter-link.blade.php
+++ resources/views/shopfegrequeststore/emails/graphic-request-submitter-link.blade.php
@@ -3,6 +3,5 @@
 <b>Submitter:</b> {{$submitter}}<br>
 <b>Location:</b> {{$location_id}} | {{$location_name}}<br>
 <b>Description:</b> {{$description}} <a href="{{$request_link}}">See full request</a><br>
-Please click on <a href="{{$approve_link}}">Approval</a> or <a href="{{$deny_link}}">Denial</a> <br>
-to Approve/Deny this graphic request <br><br>&nbsp;&nbsp;&nbsp;
+Please click on <a href="{{$approve_link}}">Approval</a> or <a href="{{$deny_link}}">Denial</a> to Approve/Deny this graphic request <br><br>
 <br> To fast-track the completion of this task, please contact the Graphics Department at (847) 852-4270 to arrange an expedited deadline.
