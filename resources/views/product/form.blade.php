8a3537c15060ff0cb093b12ec8a84f79bba19c52
adnan <shayansolutions@gmail.com>
2017-06-01 11:20:45 +0500

    [fixing sub type can't populate in product]

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -87,17 +87,7 @@
 
                         </div>
                     </div>
-                    <div class="form-group  " >
-                        <label for="Expense Category" class=" control-label col-md-4 text-left">
-                            {!! SiteHelpers::activeLang('Expense Category', (isset($fields['expense_category']['language'])? $fields['expense_category']['language'] : array())) !!}
-                        </label>
-                        <div class="col-md-6">
-                            {!! Form::text('expense_category', $row['expense_category'],array('class'=>'form-control', 'placeholder'=>'','parsley-type'=>"number", 'required'=>'true'  )) !!}
-                        </div>
-                        <div class="col-md-2">
 
-                        </div>
-                    </div>
                     <div class="form-group  ">
                         <label for="Quantity Per Case" class=" control-label col-md-4 text-left">
                             {!! SiteHelpers::activeLang('Quantity Per Case', (isset($fields['num_items']['language'])?
@@ -143,7 +133,17 @@
 
                         </div>
                     </div>
+                    <div class="form-group  " >
+                        <label for="Expense Category" class=" control-label col-md-4 text-left">
+                            {!! SiteHelpers::activeLang('Expense Category', (isset($fields['expense_category']['language'])? $fields['expense_category']['language'] : array())) !!}
+                        </label>
+                        <div class="col-md-6">
+                            {!! Form::text('expense_category', $row['expense_category'],array('class'=>'form-control','id'=>'expense_category', 'placeholder'=>'','parsley-type'=>"number", 'required'=>'true'  )) !!}
+                        </div>
+                        <div class="col-md-2">
 
+                        </div>
+                    </div>
 
                     <div class="form-group  ">
                         <label for="Case Price" class=" control-label col-md-4 text-left">
@@ -367,15 +367,44 @@
 
         $("#prod_type_id").jCombo("{{ URL::to('product/comboselect?filter=order_type:id:order_type:can_request:1') }}",
                 {selected_value: '{{ $row["prod_type_id"] }}'});
-
-        $("#prod_type_id").change(function () {
+        if('{{ $row["prod_type_id"] }}')
+        {
+            $("#prod_sub_type_id").jCombo("{{ URL::to('product/comboselect?filter=product_type:id:type_description') }}&parent=request_type_id:{{ $row["prod_type_id"] }}",
+                    {selected_value: '{{ $row["prod_sub_type_id"] }}'});
+        }
+        $("#prod_type_id").click(function () {
             $("#prod_sub_type_id").jCombo("{{ URL::to('product/comboselect?filter=product_type:id:type_description') }}&parent=request_type_id:"+$('#prod_type_id').val()+"",
                     {selected_value: '{{ $row["prod_sub_type_id"] }}'});
+            if($(this).val()) {
+                //need to uncomment after discussion
+                //getExpenseCategory($(this).val());
+            }
+
+         if ($(this).val() == "8") {
+             $("#retail_price").show(300);
+             $("#retail_price").attr('required','required');
+         }
+         else {
+             $("#retail_price").hide(300);
+             $("#retail_price").removeAttr('required');
+         }
+         if ($(this).val() == "7") {
+             $("#ticket_value").show(300);
+             $("#ticket_value").attr('required','required');
+         }else if($(this).val() == "8"){
+             $("#ticket_value").show(300);
+         }
+         else {
+             $("#ticket_value").hide(300);
+             $("#ticket_value").removeAttr('required');
+         }
         });
-
-
-        /*$("#prod_sub_type_id").jCombo("{{ URL::to('product/comboselect?filter=product_type:id:type_description') }}&parent=request_type_id:",
-                {parent: '#prod_type_id', selected_value: '{{ $row["prod_sub_type_id"] }}'});*/
+        $("#prod_sub_type_id").click(function () {
+            if($(this).val()) {
+                //need to uncomment after discussion
+                //getExpenseCategory($("#prod_type_id").val(),$(this).val());
+            }
+        });
 
         $("#vendor_id").jCombo("{{ URL::to('product/comboselect?filter=vendor:id:vendor_name:hide:0:status:1') }}",
                 {selected_value: '{{ $row["vendor_id"] }}' ,
@@ -437,26 +466,6 @@
         });
     });
 
-    $("#prod_type_id").click(function () {
-        if ($(this).val() == "8") {
-            $("#retail_price").show(300);
-            $("#retail_price").attr('required','required');
-        }
-        else {
-            $("#retail_price").hide(300);
-            $("#retail_price").removeAttr('required');
-        }
-        if ($(this).val() == "7") {
-            $("#ticket_value").show(300);
-            $("#ticket_value").attr('required','required');
-        }else if($(this).val() == "8"){
-            $("#ticket_value").show(300);
-        }
-        else {
-            $("#ticket_value").hide(300);
-            $("#ticket_value").removeAttr('required');
-        }
-    });
     function showRequest() {
         $('.ajaxLoading').show();
     }
@@ -485,6 +494,23 @@ $('#qty_input,#case_price_input').on('keyup',function(){
         $('#unit_price_input').val(0.000);
     }
 });
+    $("#prod_type_id").click(function () {
+
+    });
+    function getExpenseCategory(order_type_id,product_type_id)
+    {
+        if(product_type_id === null)
+        {
+            product_type_id="";
+        }
+        $.get('product/expense-category',{'order_type':order_type_id,'product_type':product_type_id},function(data){
+            if(data.expense_category)
+            {
+
+                $("#expense_category").val(data.expense_category);
+            }
+        },'json');
+    }
 </script>
 <style>
     #ticket_value, #retail_price {
