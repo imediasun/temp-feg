94cda7edad5e44e7b517cf373e82d9b198b03a71
Arslan Ali <shayansolutions@gmail.com>
2017-06-03 11:10:23 +0500

    [Email] Enabling gmail OAuth email only for freight Quote module

--- app/Http/Controllers/ManagefreightquotersController.php
+++ app/Http/Controllers/ManagefreightquotersController.php
@@ -597,7 +597,7 @@ class ManagefreightquotersController extends Controller
                         'to' => $to,
                         'subject' => $subject,
                         'message' => $message,
-//                        'preferGoogleOAuthMail' => true,
+                        'preferGoogleOAuthMails' => true,
                         'isTest' => env('APP_ENV', 'development') !== 'production' ? true : false,
                         'from' => $from,
                         'bcc' => $bcc,
