997afd5f50b31fb1504f4a7312aee5f9ec837acf
Arslan Ali <shayansolutions@gmail.com>
2017-06-03 12:46:06 +0500

    Revert "[Email] Fixing typo in configuration name"
    This reverts commit e8da75a291b79ca3a5aaf133853a32a2d2d0723d.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -850,7 +850,7 @@ class OrderController extends Controller
         FEGSystemHelper::sendSystemEmail(array_merge($receipts, array(
             'subject' => $subject,
             'message' => $message,
-//            'preferGoogleOAuthMails' => true,
+//            'preferGoogleOAuthMail' => true,
             'isTest' => env('APP_ENV', 'development') !== 'production' ? true : false,
             'configName' => $configName,
             'from' => $from,
