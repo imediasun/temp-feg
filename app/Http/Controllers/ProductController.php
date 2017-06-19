eba4238fe367e669a5a89caaf4ce5c0412bde866
Asad Mahmood <intern2@shayansolutions.com>
2017-06-19 12:07:16 +0500

    not saving / updating product if anouther product with same data exist

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -287,7 +287,7 @@ class ProductController extends Controller
             $retail_price=0.000;
         }
         if ($validator->passes()) {
-            $alreadyExist = Product::where('vendor_description',$data['vendor_description'])->where('case_price',$data['case_price'])->where('vendor_id',$data['vendor_id'])->where('product_type_id',$data['product_type_id'])->where('product_sub_type_id',$data['product_sub_type_id'])->first();
+            $alreadyExist = Product::where('vendor_description',$request->vendor_description)->where('case_price',$request->case_price)->where('vendor_id',$request->vendor_id)->where('product_type_id',$request->product_type_id)->where('product_sub_type_id',$request->product_sub_type_id)->first();
             if(is_object($alreadyExist))
             {
                 return response()->json(array(
