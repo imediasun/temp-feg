839555cca09abe4270f1267b5b4eecde4a681672
Haider Ali <dev1@shayansolutions.com>
2017-06-20 16:59:13 +0500

    cart update fix for ie

--- resources/views/addtocart/table.blade.php
+++ resources/views/addtocart/table.blade.php
@@ -206,7 +206,7 @@
                    <div class="col-md-10 col-sm-9 col-xs-12">
                     <input type="button" style="font-weight: bold;" class="btn btn-sm btn-success"
                            value="Submit Weekly Requests totalling {{CurrencyHelpers::formatPrice($cartData['shopping_cart_total'])}}"
-                           onClick="confirmSubmit();" id = "cartbtn"></button>
+                           onClick="confirmSubmit({{ json_encode($cartData['amt_short_message']) }});" id = "cartbtn"></button>
                 </div>
             </div>
             </div>
@@ -313,75 +313,7 @@
         $('#new_locationdiv').hide();
     });
     -->
-    var timer = null;
 
-
-    function doStuff(value,id,vendor_name) {
-        $.ajax({
-            url:"addtocart/save/"+id+"/"+value+"/"+encodeURIComponent(vendor_name) ,
-            method:'get',
-            dataType:'json',
-            success:function(data){
-                loadCart(vendor_name,data.subtotal);
-            },
-            error: function(){
-                unblockUI();
-            },
-
-        });
-    }
-    timer=null;
-    function changeTotal(value,id,e)
-    {
-        var vendor_name1=$("#"+id).data('vendor');
-       vendor_name1=vendor_name1.replace(/ /g, '_');
-        if (e.keyCode == 13 && value > 0) {
-            $('.ajaxLoading').show();
-            e.preventDefault();
-            doStuff(value,id,vendor_name1);
-        }
-      
-    }
-    function confirmSubmit() {
-        var shortMessage;
-        if(amt_short_msg == null) {
-            shortMessage = "{{ json_encode($cartData['amt_short_message']) }}";
-}
-        else
-        {
-     shortMessage=amt_short_msg;
-        }
-        shortMessage=shortMessage.replace(/&quot;/g, '');
-            shortMessage=shortMessage.trim();
-        if ( shortMessage && shortMessage.length > 0 ) {
-
-            var text="Please increase order amount in order to proceed."
-            alert(shortMessage +" "+ text);
-
-        }
-        else {
-            if (confirm("Have you confirmed all items and quantities in your shopping cart?")) {
-                var new_location = '';
-                var checked = $("#clone_order").parent('[class*="icheckbox"]').hasClass("checked");
-
-                if (checked) {
-                    if ($('#new_location').val() > 0) {
-                        new_location = '/' + $('#new_location').val();
-                        window.location.href = '{{ $pageModule }}/submit-requests' + new_location;
-                    }
-                    else {
-                        alert("You must pick a location to clone this order to!");
-                    }
-                }
-                else {
-                 //   alert('{{ $pageModule }}/submit-requests');
-                    window.location.href = '{{ $pageModule }}/submit-requests';
-                    $("#update_text_to_add_cart").text('0');
-                }
-            }
-
-        }
-    }
 
 </script>
 <style>
