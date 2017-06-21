c4866651839cd0625954b0ba37c40e9ba7e000c9
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-06-21 19:22:15 +0530

    [fix][search] trim only strings

--- public/sximo/js/simple-search.js
+++ public/sximo/js/simple-search.js
@@ -35,7 +35,9 @@ function performSimpleSearch(params) {
         if (value === null || value === UNDEFINED ) {
             value = '';
         }
-        value = $.trim(value);
+        if (typeof value == 'string') {
+            value = $.trim(value);
+        }
         // not required to be included
         if (!fieldName || fieldName === '_token' || value === '') {
             return;
