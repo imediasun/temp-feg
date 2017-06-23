87115eb5225f27bb4da3df3a2a12d609e1aa1ae3
Haider Ali <dev1@shayansolutions.com>
2017-06-23 14:35:33 +0500

    PO restore test

--- app/Console/Commands/RestorePONumber.php
+++ app/Console/Commands/RestorePONumber.php
@@ -37,8 +37,6 @@ class RestorePONumber extends Command
      */
     public function handle()
     {
-        \Log::info("Running Unused PO's Restored.");
-
         if (!env('AUTOMATIC_UNUSED_PO_RESTORE', false)) {
             return;
         }
