531412527faaeec01d656e07df2ff78169fea8d3
SudiptoChoudhury <sudiptoc@inmedianetworks.com>
2017-06-28 03:33:25 +0530

    [tickets] [refactor] implement direct view link for tickets embedded into ticket email [cosmetics]

--- resources/views/servicerequests/email/commentviewlink.blade.php
+++ resources/views/servicerequests/email/commentviewlink.blade.php
@@ -1,9 +1,9 @@
 <?php
     $ticketLink = "font-size: 11px;
-                margin:20px 0 40px;
+                margin:40px 0;
                 padding: 10px;
-                border-top:1px solid #aaa;
-                border-bottom:1px solid #aaa;
-                color: #999;";
+                border-top:1px solid #ccc;
+                border-bottom:1px solid #ccc;
+                color: #777;";
 ?>
 <div style="{!! $ticketLink !!}">View Service Request: {{ $url }}</div>
