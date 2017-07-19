dbc3423491a8c32639aa13e06a43c6ed9fae50a1
Haider Ali <dev1@shayansolutions.com>
2017-07-19 13:51:38 +0500

    Debit type column in Service Requests

--- app/Models/Servicerequests.php
+++ app/Models/Servicerequests.php
@@ -24,7 +24,7 @@ class Servicerequests extends Observerable  {
                     IF(ISNULL(sbc.Posted),
                         DATEDIFF('$date', sb_tickets.Created),
                         DATEDIFF('$date', sbc.Posted)) AS last_updated_elapsed_days,
-                    sb_tickets.*
+                    sb_tickets.*, D.company as debit_type
 
                 FROM sb_tickets
 
@@ -36,7 +36,10 @@ class Servicerequests extends Observerable  {
 				WHERE tcm.max_posted = sb_ticketcomments.Posted
     ) sbc ON sbc.TicketID = sb_tickets.TicketID
 
-	LEFT JOIN users U ON U.id = sbc.UserID";
+	LEFT JOIN users U ON U.id = sbc.UserID
+	INNER JOIN location L ON ( sb_tickets.location_id = L.id )
+    INNER JOIN debit_type D ON (L.debit_type_id = D.id)
+	";
 
         return $sql;
     }
