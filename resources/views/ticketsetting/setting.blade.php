e6fe6977648ee75c288e8a20a17da5bd066a3606
Asad Mahmood <intern2@shayansolutions.com>
2017-08-09 17:30:32 +0500

    task 91 updated users with any other group except super admin will not able to add remove admin from service request settings and from CMS pages

--- resources/views/ticketsetting/setting.blade.php
+++ resources/views/ticketsetting/setting.blade.php
@@ -117,6 +117,9 @@
 
     <script>
         $(document).ready(function(){
+            superAdmin = {{\App\Models\Core\Groups::SUPPER_ADMIN}};
+            alert(superAdmin);
+            alert({{Session::get('gid')}});
             $("#role1").jCombo("{{ URL::to('sbticket/comboselect?filter=tb_groups:group_id:name') }}",
                     {selected_value: '{{ $ticket_setting->role1 }}'});
             $("#role2").jCombo("{{ URL::to('sbticket/comboselect?filter=tb_groups:group_id:name') }}",
@@ -138,11 +141,19 @@
             $("#individual5").jCombo("{{ URL::to('sbticket/comboselect?filter=users:id:first_name|last_name') }}",
                     {selected_value: '{{ $ticket_setting->individual5 }}'});
         });
-
+        @if(Session::get('gid') != \App\Models\Core\Groups::SUPPER_ADMIN)
+                    $( document ).ajaxStop(function() {
+            console.log( "Triggered ajaxStop handler." );
+            $('#role1 option[value="'+superAdmin+'"],#role2 option[value="'+superAdmin+'"],#role3 option[value="'+superAdmin+'"],#role4 option[value="'+superAdmin+'"],#role5 option[value="'+superAdmin+'"]').attr('disabled','disabled');
+            $('#role1,#role2,#role3,#role4,#role5').trigger('change');
+        });
+                @endif
         var form = $('#sbticketSetting');
         form.parsley();
         form.submit(function () {
             if (form.parsley('isValid') == true) {
+                $('#role1 option[value="'+superAdmin+'"],#role2 option[value="'+superAdmin+'"],#role3 option[value="'+superAdmin+'"],#role4 option[value="'+superAdmin+'"],#role5 option[value="'+superAdmin+'"]').removeAttr('disabled');
+                $('#role1,#role2,#role3,#role4,#role5').trigger('change');
                 var options = {
                     dataType: 'json',
                     beforeSubmit: showRequest,
