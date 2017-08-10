8ba17ce6763a9a06e76e372505b506b14f6375b6
Asad Mahmood <intern2@shayansolutions.com>
2017-08-10 16:07:40 +0500

    task 31 an order can only be received if it cannot be post to net suite. updated

--- resources/views/order/table.blade.php
+++ resources/views/order/table.blade.php
@@ -187,7 +187,7 @@ usort($tableGrid, "SiteHelpers::_sort");
                         $canPostToNetSuit = Order::canPostToNetSuit($row->id);
                         $isApified = Order::isApified($id, $row);
                         ?>
-                        @if((!$canPostToNetSuit && !$isApified) || $row->status_id !='Close')
+                        @if((!$canPostToNetSuit && !$isApified) || $row->status_id !='Close Order')
                             <a href="{{ URL::to('order/orderreceipt/'.$row->id)}}"
                                data-id="{{$eid}}"
                                data-action="receipt"
