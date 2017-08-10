8507d1441be5920ebfa14998d90118d10cf9abc9
Asad Mahmood <intern2@shayansolutions.com>
2017-08-10 17:44:52 +0500

    task 94 removing html tags from csv / excel reports updated

--- resources/views/sximo/module/utility/excel_mylocationgames.php
+++ resources/views/sximo/module/utility/excel_mylocationgames.php
@@ -23,13 +23,13 @@
 				}
 				unset($f['attribute']['hyperlink']);
 				$conn = (isset($f['conn']) ? $f['conn'] : array() );
-                $a = htmlentities(AjaxHelpers::gridFormater($row->$f['field'],$row,$f['attribute'],$conn));
+                $a = (AjaxHelpers::gridFormater($row->$f['field'],$row,$f['attribute'],$conn));
                 $b = str_replace( ',', '', $a );
                 $c = str_replace('$','',$b);
                 if( is_numeric( $c ) ) {
                     $a = $c;
                 }
-                $content .= '<td> '. strip_tags(html_entity_decode($a)) . '</td>';
+                $content .= '<td> '. strip_tags(($a)) . '</td>';
 			endif;
 		}
 		$content .= '</tr>';
