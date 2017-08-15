d366443f85acc6ac4d1392990fdb59bfb29b943d
Haider Ali <dev1@shayansolutions.com>
2017-08-15 19:36:41 +0500

    Bug#37 logo click able

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -465,7 +465,13 @@
             }
         });
 
-
+        $('#ordersubmitFormAjax').on('keyup keypress', function(e) {
+            var keyCode = e.keyCode || e.which;
+            if (keyCode === 13) {
+                e.preventDefault();
+                return false;
+            }
+        });
         var isRequestApprovalProcess = <?php echo $isRequestApproveProcess ? 'true' : 'false'; ?>;
         var counter = isRequestApprovalProcess ? $('input[name^=item_num]').length : 0;
         var hidePopup;
@@ -481,6 +487,10 @@
             $('body #sidemenu a:not(.expand)').on('click',function (e) {
                 e.preventDefault();
                 reloadOrder(1);
+                if($(this).attr('id') == 'logo')
+                {
+                    location.href = $(this).attr('href');
+                }
             });
             $('.navbar-top-links li a:not([href="javascript:void(0)"])').on('click',function(e)
             {
