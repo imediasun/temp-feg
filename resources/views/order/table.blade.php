c510c8217081f7a066d53659ada5c359a78b66f1
Haider Ali <dev1@shayansolutions.com>
2017-08-16 15:26:23 +0500

    Bug#109 (cherry picked from commit ebaba8c)

--- resources/views/order/table.blade.php
+++ resources/views/order/table.blade.php
@@ -374,7 +374,7 @@ usort($tableGrid, "SiteHelpers::_sort");
                     if(data.status === 'success'){
                         notyMessage(data.message);
                         btn.remove();
-                        reloadData('#{{ $pageModule }}', '{{ $pageModule }}/data');
+                        //reloadData('#{{ $pageModule }}', '{{ $pageModule }}/data');
                     }
                     else {
                         btn.prop('disabled', false);
