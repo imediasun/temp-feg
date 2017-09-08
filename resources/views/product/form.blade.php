67636e9318dc7921fb04368293f0d5cbd4d36c97
Haider Ali <dev1@shayansolutions.com>
2017-09-08 11:22:45 +0500

    merge for bug#98

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -486,14 +486,10 @@
         $('.editor').summernote();
         $('.previewImage').fancybox();
         $('.tips').tooltip();
-        renderDropdown($(".select2"), {width: "100%"});
+
 
         $('.date').datepicker({format: 'mm/dd/yyyy', autoclose: true})
         $('.datetime').datetimepicker({format: 'mm/dd/yyyy hh:ii:ss'});
-        $('input[type="checkbox"],input[type="radio"]').not('.test').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue'
-        });
         $('.removeCurrentFiles').on('click', function () {
             var removeUrl = $(this).attr('href');
             $.get(removeUrl, function (response) {
