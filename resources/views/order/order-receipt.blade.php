83107a1ab4e409d597fb423cc3883ecaa2f4feb3
Haider Ali <dev1@shayansolutions.com>
2017-10-02 19:29:44 +0500

    Total cost hotfix

--- resources/views/order/order-receipt.blade.php
+++ resources/views/order/order-receipt.blade.php
@@ -51,7 +51,6 @@
                             <tr><td><b>Total Cost:</b></td><td>{{ CurrencyHelpers::formatPrice($data['order_total'],\App\Models\Order::ORDER_PERCISION ) }}</td></tr>
                             <tr><td><b></b></td> <td> {{--<button type="button" class="btn btn-primary btn-sm" data-toggle="collapse" data-target="#editItemsPan" style="float: right;margin-top: 19px;" id="edit_receipt_btn"><i class="fa fa-edit"></i> Edit Receipt</button>--}}
                                    <input type='checkbox' name="toggle_trigger" data-handle-width="100px" data-size="mini" data-on-text="Edit Receipt" data-off-text="Receive Items" id="toggle_trigger" onSwitchChange="trigger()" /> </td></tr>
-                            <tr><td><b>Total Cost:</b></td><td>{{ CurrencyHelpers::formatPrice( $data['order_total'],\App\Models\Order::ORDER_PERCISION ) }}</td></tr>
                             <?php //if(!empty($item_count) && ($order_type == 7 || $order_type == 8) && () && $added_to_inventory == 0)  //REDEMPTION OR INSTANT WIN PRIZES -  SET TO DUMMY VALUE TO FORCE ORDER DESCRIPION UNTIL WE INTRODUCE PRIZE ALLOCATION
                             ?>
                             @if((isset($data['item_count']) && !empty($data['item_count'])) && ($data['order_type'] == 7 || $data['order_type'] == 8) &&   $data['added_to_inventory'] == 0)  //REDEMPTION OR INSTANT WIN PRIZES -  SET TO DUMMY VALUE TO FORCE ORDER DESCRIPION UNTIL WE INTRODUCE PRIZE ALLOCATION
