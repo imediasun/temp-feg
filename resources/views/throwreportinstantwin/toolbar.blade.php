8071a6ba3acc330e31416efaceeaf4eedc5131c2
Junaid <marslan.ali@gmail.com>
2017-10-20 11:17:44 +0500

    [Select Column Arrangement] field pixel issue is resolved

--- resources/views/throwreportinstantwin/toolbar.blade.php
+++ resources/views/throwreportinstantwin/toolbar.blade.php
@@ -5,16 +5,18 @@
 
         @endif
         @if($access['is_remove'] ==1)
-        <a href="javascript://ajax" class="btn btn-sm btn-white" onclick="ajaxRemove('#{{ $pageModule }}','{{ $pageUrl }}');"><i class="fa fa-trash-o "></i> {{ Lang::get('core.btn_remove') }} </a>
+        <a href="javascript://ajax" class="btn btn-sm btn-white float-margin" onclick="ajaxRemove('#{{ $pageModule }}','{{ $pageUrl }}');"><i class="fa fa-trash-o "></i> {{ Lang::get('core.btn_remove') }} </a>
         @endif
         @endif
         <!--<a href="{{ URL::to( $pageModule .'/search') }}" class="btn btn-sm btn-white" onclick="SximoModal(this.href,'Advanced Search'); return false;" ><i class="fa fa-search"></i>Advanced Search</a>-->
-            <label>Week Date Range</label>
+           <div class="float-margin">
+               <label>Week Date Range</label>
             <input type="text" class="weeklyDatePicker"  name ="weeklyDatePicker"  style="padding-bottom:5px" } />
+               </div>
         @if(SiteHelpers::isModuleEnabled($pageModule))
-        <a id="edit-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule) }}" class="btn btn-sm btn-white" onclick="SximoModal(this.href,'Arrange Columns'); return false;" ><i class="fa fa-bars"></i> Arrange Columns</a>
+        <a id="edit-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule) }}" class="btn btn-sm btn-white float-margin" onclick="SximoModal(this.href,'Arrange Columns'); return false;" ><i class="fa fa-bars"></i> Arrange Columns</a>
         @if(!empty($colconfigs))
-        <select class="form-control" style="width:auto!important;display:inline;" name="col-config"
+        <select class="form-control float-margin height-set" style="width:auto!important;display:inline;" name="col-config"
                 id="col-config">
             <option value="0">Select Column Arrangement</option>
             @foreach( $colconfigs as $configs )
@@ -22,7 +24,7 @@
             @endif value={{ $configs['config_id'] }}> {{ $configs['config_name'] }}   </option>
             @endforeach
         </select>
-                            <a id="edit-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule.'/edit') }}" class="btn btn-sm btn-white"
+                            <a id="edit-cols" href="{{ URL::to('tablecols/arrange-cols/'.$pageModule.'/edit') }}" class="float-margin btn btn-sm btn-white"
                                onclick="SximoModal(this.href,'Arrange Columns'); return false;"><i class="fa fa-bars"></i> Edit Columns Arrangement</a>
 
         @else
