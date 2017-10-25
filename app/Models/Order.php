f660e1978b870583628eaa0190b3ccc8b4597230
Junaid <marslan.ali@gmail.com>
2017-10-25 20:07:46 +0500

    bug-194 issue resolved

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -36,6 +36,11 @@ class order extends Sximo
                 LEFT OUTER JOIN order_status OS ON orders.status_id=OS.id
                 LEFT OUTER JOIN yes_no YN ON orders.is_partial=YN.id";
     }
+    public static function getProductInfo($id){
+        $select ="SELECT CONCAT('(',order_contents.qty,') ',order_contents.item_name,' $',order_contents.total) AS info FROM order_contents WHERE order_id = ".$id;
+        $result = \DB::select($select );
+        return $result;
+    }
 
     public static function processApiData($json,$param=null)
     {
