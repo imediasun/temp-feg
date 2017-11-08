54aaee214790ae0eb6136b43a2a733e9333ff4ad
Junaid <marslan.ali@gmail.com>
2017-11-08 15:33:10 +0500

    location filter in advance search is added (Employee module)

--- app/Models/Core/Users.php
+++ app/Models/Core/Users.php
@@ -18,9 +18,10 @@ class Users extends Sximo  {
 
 	public static function querySelect(  ){
 		
-		return " SELECT  users.*,  tb_groups.name, users.group_id, users.username,
+		return " SELECT  users.*,  tb_groups.name, users.group_id, users.username,user_locations.location_id,
                 IF(has_all_locations = 0,(SELECT GROUP_CONCAT(DISTINCT location_name SEPARATOR ', ') FROM user_locations JOIN location ON location.id = user_locations.location_id WHERE user_id = users.id GROUP BY user_id) ,\"All Locations\") AS has_all_locations
-                FROM users LEFT JOIN tb_groups ON tb_groups.group_id = users.group_id ";
+                FROM users LEFT JOIN tb_groups ON tb_groups.group_id = users.group_id
+                LEFT JOIN user_locations ON user_locations.user_id = users.id ";
 	}	
 
 	public static function queryWhere( $id = null ){
