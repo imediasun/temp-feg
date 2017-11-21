7300eaaa4632eec4e861b262ad3e60e2158b69b5
Junaid <marslan.ali@gmail.com>
2017-11-21 13:13:22 +0500

    currency helper changed to remove the space from price and $ sign in Product Names

--- app/Library/CurrencyHelpers.php
+++ app/Library/CurrencyHelpers.php
@@ -26,8 +26,14 @@ class CurrencyHelpers
 
          return '$ '.$value;
      }
-     public static function formatPrice($value,$decimalPlaces=3,$isDollarSign=true, $thousands_sep = ',',$dec_point = '.'  ){
-         $formattedValue= ($isDollarSign === true)?'$ ':"";
+     public static function formatPrice($value,$decimalPlaces=3,$isDollarSign=true, $thousands_sep = ',',$dec_point = '.' , $dolar_space = false ){
+
+         if($dolar_space === false){
+             $formattedValue= ($isDollarSign === true)?'$ ':"";
+         }else{
+             $formattedValue= ($isDollarSign === true)?'$':"";
+         }
+
          $formattedValue .= number_format((float)$value,$decimalPlaces,$dec_point , $thousands_sep);
          return $formattedValue;
      }
