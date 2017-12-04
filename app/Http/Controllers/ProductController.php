2be42ede680d57e1c43f221e74cc6c6cb2893775
Dev3 <shayansolutions@gmail.com>
2017-12-04 12:44:47 +0500

    bug-98-extended resolved active / inactive issue for edit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -386,6 +386,9 @@ class ProductController extends Controller
                         unset($data_attached_products['expense_category']);
                         unset($data_attached_products['retail_price']);
                         unset($data_attached_products['ticket_value']);
+                        unset($data_attached_products['inactive']);
+                        unset($data_attached_products['in_development']);
+                        unset($data_attached_products['hot_item']);
 
                         $this->model->insertRow($data_attached_products,$pc->id);
                     }
