fa227ad1e00de09b7adb574b45555d119c74a2b4
Arslan Ali <marslan.ali@gmail.com>
2017-12-12 19:48:07 +0500

    Bug 226 - Employees added to Svc Req not receiving emails due to location

--- app/Models/Core/TicketMailer.php
+++ app/Models/Core/TicketMailer.php
@@ -102,7 +102,6 @@ class TicketMailer
     protected function getFollowersEmails($followerIDs = array(), $locationId = null) {
         $emails = [];
         if (!empty($followerIDs)) {
-            // locationId set as null
             $emails = FEGSystemHelper::getUserEmails(implode(',',$followerIDs), null, true);
         }        
         return array_diff(array_unique($emails), ['', null]);
