f6b2c2810567c0b09db7a57f1a508b7c8bf93d42
Arslan Ali <shayansolutions@gmail.com>
2017-12-19 20:17:58 +0500

    fixing bug 232 Changing game title alias from T to game_title

--- app/Models/Gamesintransit.php
+++ app/Models/Gamesintransit.php
@@ -18,20 +18,20 @@ class gamesintransit extends Sximo
     public static function querySelect()
     {
 
-        return "SELECT game.*,IF(game.test_piece = 1, CONCAT('**TEST** ',T.game_title),T.game_title) as game_name,game.game_name as gname,
+        return "SELECT game.*,IF(game.test_piece = 1, CONCAT('**TEST** ',game_title.game_title),game_title.game_title) as game_name,game.game_name as gname,
                IF(game.location_id = 0, '<em style=\"color:red\">IN TRANSIT</em>', L1.location_name_short), CONCAT(L1.id,' | ',L1.location_name_short) as location,
-               IF(game.intended_first_location IS NULL OR game.intended_first_location=0, '<em style=\"color:red\">Check Notes to Confirm</em>', CONCAT(L3.id, ' | ',L3.location_name_short)) as intended_first_location,T.img,
-               IF(T.game_title LIKE '%*TEST*%','', CONCAT('../qr/',game.id,'.png')),If(L2.id IS NULL, 'NEW GAME' , CONCAT(L2.id,' | ',L2.location_name_short)) ,
-               game_status.game_status,If(Y.game_type IS NULL, '' , Y.game_type) ,If(V.vendor_name IS NULL, '' , V.vendor_name),T.game_type_id,T.img
+               IF(game.intended_first_location IS NULL OR game.intended_first_location=0, '<em style=\"color:red\">Check Notes to Confirm</em>', CONCAT(L3.id, ' | ',L3.location_name_short)) as intended_first_location,game_title.img,
+               IF(game_title.game_title LIKE '%*TEST*%','', CONCAT('../qr/',game.id,'.png')),If(L2.id IS NULL, 'NEW GAME' , CONCAT(L2.id,' | ',L2.location_name_short)) ,
+               game_status.game_status,If(Y.game_type IS NULL, '' , Y.game_type) ,If(V.vendor_name IS NULL, '' , V.vendor_name),game_title.game_type_id,game_title.img
                From game
                LEFT JOIN location L1 ON (game.location_id = L1.id)
 			   LEFT JOIN location L2 ON (game.prev_location_id = L2.id)
 			   LEFT JOIN location L3 ON (game.intended_first_location = L3.id)
 			   LEFT JOIN region ON (L1.region_id = region.id)
 			   LEFT JOIN game_status ON (game.status_id = game_status.id)
-			   LEFT JOIN game_title T ON (game.game_title_id = T.id)
-			   LEFT JOIN game_type Y ON (T.game_type_id = Y.id)
-			   LEFT JOIN vendor V ON (T.mfg_id = V.id)";
+			   LEFT JOIN game_title  ON (game.game_title_id = game_title.id)
+			   LEFT JOIN game_type Y ON (game_title.game_type_id = Y.id)
+			   LEFT JOIN vendor V ON (game_title.mfg_id = V.id)";
     }
 
     public static function queryWhere()
