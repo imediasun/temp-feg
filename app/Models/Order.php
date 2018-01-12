0351681b63c2c9615629e4b42cbbabfb4ae2f9be
Arslan Ali <shayansolutions@gmail.com>
2018-01-12 21:28:24 +0500

    [Debug] Feg API

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -87,7 +87,8 @@ class order extends Sximo
     {
         //$poNotes = preg_replace("/[\r\n]+/", " ", $poNotes);
         $poNotes = str_replace(["\r\n","t"]," ",$poNotes);
-        $poNotes = preg_replace('/\t+\n+/', '', $poNotes);
+        $poNotes = preg_replace('/\t+/', '', $poNotes);
+        $poNotes = preg_replace('/\n+/', '', $poNotes);
         if(empty($poNotes) || strlen($poNotes) < $length){
             return $poNotes;
         }
