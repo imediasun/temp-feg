1a2d3e7a78600c759a0f9ffc01ca4bad4eae1174
Arslan Ali <marslan.ali@gmail.com>
2018-01-26 16:12:09 +0500

    Bug 239 - Netsuite Live > Sandbox Order Sync Issues

--- app/Models/Itemreceipt.php
+++ app/Models/Itemreceipt.php
@@ -236,6 +236,7 @@ orders.id=order_received.order_id ";
                 if ($data->id == $record->order_id) {
                     if(in_array($record->order_type_id,$order_types)){
                         $record->quantity = $record->quantity*$record->qty_per_case;
+                        $record->quantity='DDD';
                     }
                     $result[$record->order_id]['receipts'][] = [
                         'id' => $record->id,
