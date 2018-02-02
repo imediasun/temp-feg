82d8e117e20a420e59415c8e8a459c40861f0869
Arslan Ali <marslan.ali@gmail.com>
2018-02-02 12:31:36 +0500

    Bug-241 reserved quantity adjustment on edit orders

--- app/Models/ReservedQtyLog.php
+++ app/Models/ReservedQtyLog.php
@@ -2,15 +2,23 @@
 
 namespace App\Models;
 
+use App\Http\Controllers\OrderController;
+use App\Models\Sximo\Module;
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Ordertyperestrictions;
+use Illuminate\Support\Facades\DB;
+use Log;
 
-class ReservedQtyLog extends Model
+class ReservedQtyLog extends Sximo
 {
     protected $table = 'reserved_qty_log';
     protected $primaryKey = 'id';
     const TYPE = 1;
 
     public function __construct() {
+        ini_set('memory_limit','1G');
+        set_time_limit(0);
         parent::__construct();
 
     }
