8421c38c27608d94db994e65ba448ff15f565a77
Arslan Ali <marslan.ali@gmail.com>
2018-02-07 18:42:12 +0500

    bug-241 fix merging error

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -553,16 +553,16 @@ class OrderController extends Controller
     {
         $item_names = $request->input('item_name');
         $productInformation = [];
-        for($i=0; $i<count($item_names); $i++){
-            $product = \DB::table('products')->where(['id' => $request->input('product_id')[$i],'is_reserved'=>1])->first();
-            if(!empty($product)) {
-                $product->item_name=$item_names[$i];
-                $product->qty=$request->input('qty')[$i];
+        for($i=0; $i<count($item_names); $i++) {
+            $product = \DB::table('products')->where(['id' => $request->input('product_id')[$i], 'is_reserved' => 1])->first();
+            if (!empty($product)) {
+                $product->item_name = $item_names[$i];
+                $product->qty = $request->input('qty')[$i];
                 $product->changed_qty = $request->input('qty')[$i] - $request->input('prev_qty')[$i];
-                $product->order_product_id = ($request->input('product_id')[$i]==$product->id) ? $request->input('product_id')[$i] : 0;
-                $productInformation[]=$product;
+                $product->order_product_id = ($request->input('product_id')[$i] == $product->id) ? $request->input('product_id')[$i] : 0;
+                $productInformation[] = $product;
             }
-
+        }
 
         $query = \DB::select('SELECT R.id FROM requests R LEFT JOIN products P ON P.id = R.product_id WHERE R.location_id = "' . (int)$request->location_id . '"  AND P.vendor_id = "' . (int)$request->vendor_id . '" AND R.status_id = 1');
 
