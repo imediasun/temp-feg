7bd34a9e7b904eefe23bebdc910478606271d19f
Arslan Ali <marslan.ali@gmail.com>
2018-02-08 19:19:44 +0500

    Bug-261 cms page route issue

--- app/Http/pageroutes.php
+++ app/Http/pageroutes.php
@@ -13,7 +13,5 @@ Route::get('inventor', 'HomeController@index');
 Route::get('generaltips', 'HomeController@index');
 Route::get('trainingmatrials', 'HomeController@index');
 Route::get('popupblocker', 'HomeController@index');
-Route::get('testpagecms', 'HomeController@index');
-Route::get('demo-testing-page', 'HomeController@index');
 Route::get('expanse-report', 'HomeController@index');
 ?>
