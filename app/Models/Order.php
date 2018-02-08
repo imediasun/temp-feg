0706b61201e6c0144fa80f5c8dfcfefa6ad31013
Arslan Ali <marslan.ali@gmail.com>
2018-02-08 19:48:50 +0500

    temp

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -109,8 +109,7 @@ class order extends Sximo
             $orderId = $item->order_id;
 
             if(in_array($item->order_type_id,$order_types) && (!empty($item->qty_per_case) && $item->qty_per_case>0)){
-               // $item->price = \CurrencyHelpers::formatPrice(($item->case_price/$item->qty_per_case), 5, false);
-                $item->price = $item->case_price/$item->qty_per_case;
+                $item->price = \CurrencyHelpers::formatPrice(($item->case_price/$item->qty_per_case), 5, false);
                 $item->case_price = \CurrencyHelpers::formatPrice($item->case_price, 5, false);
             }else{
                 $item->price = \CurrencyHelpers::formatPrice($item->price, 5, false);
