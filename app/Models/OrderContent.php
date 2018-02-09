7077bd9891b027497f948ec051f42db2eefc6593
Arslan Ali <marslan.ali@gmail.com>
2018-02-09 15:59:50 +0500

    Bug 267 - Order status not being assigned correctly after editing a received order.

--- app/Models/OrderContent.php
+++ app/Models/OrderContent.php
@@ -1,18 +1,32 @@
-<?php namespace App\Models;
+<?php
+namespace App\Models;
 
+use App\Http\Controllers\OrderController;
+use App\Models\Sximo\Module;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Ordertyperestrictions;
+use Illuminate\Support\Facades\DB;
+use Log;
 
-class OrderContent extends Sximo  {
-
+/**
+ * Class Ordercontent
+ * @package App\Models
+ */
+class OrderContent extends Sximo
+{
+    /**
+     * @var string
+     */
     protected $table = 'order_contents';
-    protected $primaryKey = 'id';
 
-    public function __construct() {
-        parent::__construct();
-
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo("App\Models\Order");
     }
 
-    public function product(){
-        return $this->belongsTo("App\Models\product");
-    }
 
 }
