8dd724c9e2c8997962156949e7f5e3c4f0ad3286
Arslan Ali <marslan.ali@gmail.com>
2018-02-09 20:24:13 +0500

    bug-241 Visual changes

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -34,6 +34,11 @@
             width: 100%;
             margin-bottom: 14px;
         }
+        .product-reserved-qty-box{
+            float: left;
+            width: 100%;
+            margin-bottom: 14px;
+        }
     </style>
     <div class="sbox">
         <div class="sbox-title">
@@ -284,6 +289,8 @@
 
                         </div>
                     </div>
+
+                    <span class="product-reserved-qty-box" style=" {{ in_array('1',$is_reserved) ? '':'display: none;' }}">
                     <div class="form-group">
                         <label for="allow_negative_reserve_qty" class=" control-label col-md-4 text-left">
                             {!! SiteHelpers::activeLang('Allow Negative Reserved Qty', (isset($fields['allow_negative_reserve_qty']['language'])?
@@ -304,7 +311,7 @@
                     </div>
                     <div class="form-group">
                         <label for="Reserved Qty Limit" class=" control-label col-md-4 text-left">
-                            {!! SiteHelpers::activeLang('Reserved Qty Limit', (isset($fields['reserved_qty_limit']['language'])?
+                            {!! SiteHelpers::activeLang('Reserved Qty Par Amount', (isset($fields['reserved_qty_limit']['language'])?
                             $fields['reserved_qty_limit']['language'] : array())) !!}
                         </label>
 
@@ -330,6 +337,8 @@
 
                         </div>
                     </div>
+                    </span>
+
                     <div class="form-group  ">
                         <label for="Img" class=" control-label col-md-4 text-left">
                             {!! SiteHelpers::activeLang('Img', (isset($fields['img']['language'])?
@@ -460,6 +469,13 @@
         $('input[type="checkbox"],input[type="radio"]').not('.test').iCheck({
             checkboxClass: 'icheckbox_square-blue',
             radioClass: 'iradio_square-blue'
+        });
+
+        $('[name="is_reserved"]').on("ifChecked", function () {
+            $(".product-reserved-qty-box").show();
+        })
+        .on("ifUnchecked", function () {
+            $(".product-reserved-qty-box").hide();
         });
 
             $("#prod_type_id_1").jCombo("{{ URL::to('product/comboselect?filter=order_type:id:order_type:can_request:1') }}",
