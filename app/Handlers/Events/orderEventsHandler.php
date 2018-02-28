4bf25c44891c7bb069f8981517f3c2c91f615fca
Arslan Ali <marslan.ali@gmail.com>
2018-02-28 17:52:40 +0500

    Bug 241 - Reserved Product Quantities

--- app/Handlers/Events/orderEventsHandler.php
+++ app/Handlers/Events/orderEventsHandler.php
@@ -22,35 +22,35 @@ class orderEventsHandler
     /**
      * Handle the event.
      *
-     * @param  ordersEvent  $event
+     * @param  ordersEvent $event
      * @return array
      */
     public function handle(ordersEvent $event)
     {
         $error = false;
-        $ProductResponse=['error' => $error, "message" => ''];
-        $message='You have attempted to request more product than there is Reserved Quantity available. Your request has been modified to reflect this amount.';
+        $ProductResponse = ['error' => $error, "message" => ''];
+        $message = 'You have attempted to request more product than there is Reserved Quantity available. Your request has been modified to reflect this amount.';
         $adjustQty = [];
-        foreach($event->products as $product){
+        foreach ($event->products as $product) {
 
             $ReservedProductQtyLogObj = ReservedQtyLog::where('order_id', $event->order_id)
                 ->where('product_id', $product->id)
                 ->orderBy('id', 'DESC')
                 ->first();
 
-            if($ReservedProductQtyLogObj and $product->prev_qty){
+            if ($ReservedProductQtyLogObj and $product->prev_qty) {
                 $adjustmentAmount = $product->qty - $product->prev_qty;
-            }else{
+            } else {
                 $adjustmentAmount = $product->qty;
             }
 
-            if($product->allow_negative_reserve_qty == 0 && $adjustmentAmount > $product->reserved_qty){
-                $error=true;
+            if ($product->allow_negative_reserve_qty == 0 && $adjustmentAmount > $product->reserved_qty) {
+                $error = true;
                 $message .= "<br>* $product->item_name, SKU: $product->sku, Quantity: $product->reserved_qty";
-                $adjustQty[$product->id] = $product->reserved_qty < 1 ? $product->reserved_qty : $ReservedProductQtyLogObj ? $product->reserved_qty + $product->prev_qty : $product->reserved_qty;
+                $adjustQty[$product->id] = $ReservedProductQtyLogObj ? $product->reserved_qty + $product->prev_qty : $product->reserved_qty;
             }
         }
 
-       return array_merge($ProductResponse,['error' => $error, "message" => $message, "adjustQty" => $adjustQty]);
+        return array_merge($ProductResponse, ['error' => $error, "message" => $message, "adjustQty" => $adjustQty]);
     }
 }
