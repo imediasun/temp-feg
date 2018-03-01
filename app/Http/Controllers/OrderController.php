9f8ce4f6bb117dd4519cd90920ad55e9fdef891a
Arslan Ali <marslan.ali@gmail.com>
2018-03-01 16:33:10 +0500

    Bug-242 Run the script on ALL merchandise orders from 6/6/2017 and older. ...receive them ...close them ...verify them ...post them to netsuite

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2134,7 +2134,7 @@ public static function array_move($which, $where, $array)
 
     public function getCorrectOrdersBug2016($step = '1')
     {
-        die("Script blocked. To run this script please contact your development team. Thanks!");
+        // die("Script blocked. To run this script please contact your development team. Thanks!");
 
         $records = \DB::select("SELECT
               orders.id AS aa_id,
