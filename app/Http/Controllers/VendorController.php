b73807b4e90a31ea8f485f1681f92a66a2ab8d08
Arslan Ali <shayansolutions@gmail.com>
2018-03-06 14:57:11 +0500

    [Bug] Fixing very old code in vendor which was not allowing to save empty data

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -352,14 +352,8 @@ class VendorController extends Controller
         $rules["vendor_name"] = "required|unique:vendor,vendor_name," . $id;
         $validator = Validator::make($request->all(), $rules);
         if ($validator->passes()) {
-            if(empty($id))
-            {
-                $data = $this->validatePost('vendor');
-            }
-            else
-            {
-                $data = $this->validatePost('vendor', true);
-            }
+            //No one tested for 1 year that edit was not saving missing data
+            $data = $this->validatePost('vendor');
             $data['updated_by'] = \Session::get('uid');
             $data['hide'] = $request->get('hide') == "1" ?1:0;
             if (!empty($request->get('status'))) {
