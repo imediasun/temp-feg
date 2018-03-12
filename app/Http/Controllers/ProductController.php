c63da790c91808310b84b5b5f1a3e900c9b6300a
Arslan Ali <marslan.ali@gmail.com>
2018-03-12 19:26:10 +0500

    Bug 292 - Expense Category selection must by Mandatory

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -685,16 +685,22 @@ class ProductController extends Controller
 }
 
     function getExpenseCategoryGroups(){
-        $expense_category=\DB::table('expense_category_mapping')
-            ->select('mapped_expense_category as id', 'mapped_expense_category')
-            ->groupBy('mapped_expense_category')->get();
+        $expense_category = \DB::select("SELECT expense_category_mapping.id,expense_category_mapping.mapped_expense_category,order_type.`order_type`,CONCAT(mapped_expense_category,' ',GROUP_CONCAT(order_type.`order_type` ORDER BY order_type.`order_type` ASC SEPARATOR ' | ')) as order_type
+FROM expense_category_mapping
+JOIN order_type ON order_type.id = expense_category_mapping.order_type
+WHERE product_type IS NULL
+GROUP BY mapped_expense_category");
         $items = [];
-        foreach ($expense_category as $key => $category){
-            if ($category->mapped_expense_category == 0 || empty($category->mapped_expense_category))
-            {
-                // $category->mapped_expense_category = "N/A";
+        foreach ($expense_category as $category) {
+            $orderType = $category->order_type;
+            $categoryId = $category->mapped_expense_category;
+            if ($categoryId == 0) {
+                /* $orderType = "N/A";
+                 $categoryId = "";
+                */
             } else {
-                $items[] = [$category->id, $category->mapped_expense_category];
+
+                $items[] = [$categoryId, $orderType];
             }
         }
         return $items;
@@ -714,10 +720,11 @@ GROUP BY mapped_expense_category");
             $orderType = $category->order_type;
             $categoryId = $category->mapped_expense_category;
             if ($categoryId == 0) {
-                $orderType = "N/A";
-                $categoryId = "";
+                /* $orderType = "N/A";
+                 $categoryId = "";*/
+            } else {
+                $items[] = '<option value="' . $categoryId . '"> ' . $orderType . ' </option>';
             }
-            $items[] = '<option value="' . $categoryId . '"> ' . $orderType . ' </option>';
 
         }
         $options = implode("", $items);
