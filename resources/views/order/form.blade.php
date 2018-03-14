c47c21b655ef7521201348eb19daa98cf1039bfc
Arslan Ali <marslan.ali@gmail.com>
2018-03-14 16:53:38 +0500

    Bug 283 - Clicking Item Name deletes the contents of that field

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -1641,6 +1641,23 @@
                     $('.itemstable .clonedInput:first-child textarea').val('');
                 }
             }
+            if (currentElm.data('status') == 'enabled') {
+                setTimeout(function () {
+                    var item = $(".item_name").last();
+
+                    $(".item_name").last().removeAttr("onfocus", "init(this.id,this);");
+                    $('.item_name').last().removeAttr('readonly');
+
+                }, 500);
+            } else {
+                setTimeout(function () {
+                    var item = $(".item_name").last();
+
+                    $(".item_name").last().attr("onfocus", "init(this.id,this);");
+                    $('.item_name').last().removeAttr('readonly');
+
+                }, 500);
+            }
         });
     </script>
     <style>
