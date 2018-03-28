cf466669998127fad518a4680201c7a6e2be7ac5
Arslan Ali <marslan.ali@gmail.com>
2018-03-28 12:02:08 +0500

    Bug 313 - NetSuite Order's Product Status--------Test Case 2

--- app/Http/Controllers/FegapiController.php
+++ app/Http/Controllers/FegapiController.php
@@ -89,39 +89,39 @@ class FegapiController extends Controller
             if($class == 'Product'){
                 $results = $class1::getRows($param, null, null, null, true);
 
-                $results['rows'] = array_map(function ($rows) {
-                    $resh = product::find($rows->id);
-                    if ($resh) {
-                        if ($resh->inactive == 1) {
-                            $totalVariations = $resh->getProductVariations()->count();
-                            $orderedContent = $resh->orderedProduct->toArray();
-                            $ordersIds = array_map(function ($orders) {
-                                return $orders['order_id'];
-                            }, $orderedContent);
-                            $past24hours = date("Y-m-d H:i:s", strtotime("-24 hours"));
-                            // $past24hours = date("Y-m-d H:i:s",strtotime("2018-03-27 09:44:15"));
+                /* $results['rows'] = array_map(function ($rows) {
+                     $resh = product::find($rows->id);
+                     if ($resh) {
+                         if ($resh->inactive == 1) {
+                             $totalVariations = $resh->getProductVariations()->count();
+                             $orderedContent = $resh->orderedProduct->toArray();
+                             $ordersIds = array_map(function ($orders) {
+                                 return $orders['order_id'];
+                             }, $orderedContent);
+                             $past24hours = date("Y-m-d H:i:s", strtotime("-24 hours"));
+                             // $past24hours = date("Y-m-d H:i:s",strtotime("2018-03-27 09:44:15"));
 
-                            $CheckOrders = Order::whereIn("id", $ordersIds)->where("is_api_visible", "=", 1)->where("api_created_at", ">", $past24hours)->orderBy("api_created_at", "DESC")->first();
+                             $CheckOrders = Order::whereIn("id", $ordersIds)->where("is_api_visible", "=", 1)->where("api_created_at", ">", $past24hours)->orderBy("api_created_at", "DESC")->first();
 
-                            if ($CheckOrders) {
-                                if ($CheckOrders->is_api_visible == 1) {
-                                    $status = 0;
-                                    for ($i = 0; $i < $totalVariations; $i++) {
-                                        if ($i == 0) {
-                                            $status = '0';
-                                        } else {
-                                            $status .= ",0";
-                                        }
-                                    }
-                                    $rows->inactive = $status;
-                                    //return $rows;
-                                }
-                            }
+                             if ($CheckOrders) {
+                                 if ($CheckOrders->is_api_visible == 1) {
+                                     $status = 0;
+                                     for ($i = 0; $i < $totalVariations; $i++) {
+                                         if ($i == 0) {
+                                             $status = '0';
+                                         } else {
+                                             $status .= ",0";
+                                         }
+                                     }
+                                     $rows->inactive = $status;
+                                     //return $rows;
+                                 }
+                             }
 
-                        }
-                    }
-                    return $rows;
-                }, $results['rows']);
+                         }
+                     }
+                     return $rows;
+                 }, $results['rows']);*/
                 /* echo "<pre>";
                  print_r($results);
                  die;*/
