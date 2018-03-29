eecc5b31189cc770909c754e1d590e741b978979
Arslan Ali <marslan.ali@gmail.com>
2018-03-29 16:32:23 +0500

    Implementing autocomplete Test

---
+++ tests/OrderModuleTests.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class OrderModuleTests extends TestCase
+{
+    const TEST_VENDOR = '114'; //OFFICE DEPOT
+
+    public function testOrderGridPage(){
+        $this->visit('/order')
+            ->see(' FEG LLC ');
+    }
+
+    public function testGetAutoCompleteFunction(){
+        $this->get('/order?term=office&vendor_id='.self::TEST_VENDOR)
+            ->seeJson([
+                'value' => "HP1 OfficeJet Pro 8710 All-in-One Wireless Printer with Mobile Printing (M9L66A)",
+            ]);
+    }
+
+    public function testGetProductInOrderController()
+    {
+        $orderController = new \App\Http\Controllers\OrderController();
+        $products = json_decode($orderController->getProduct());
+        $this->assertFalse(empty($products));
+    }
+}
