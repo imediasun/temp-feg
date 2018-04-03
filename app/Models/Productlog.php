ed9b4af5dad929651ecda5b9afb16a1c82300696
Hamid Raza <dev3@shayansolutions.com>
2018-04-03 19:21:43 +0500

    Bug 241 - Reserved Product Quantities

--- app/Models/Productlog.php
+++ app/Models/Productlog.php
@@ -16,16 +16,14 @@ class productlog extends Sximo  {
 	public static function querySelect(  ){
 		
 		return "  SELECT
-  ''               AS search_all_fields,
-  prod.*,
-  pLOG.id,
-  pLOG.variation_id,
-  pLOG.created_at
+  ''                AS search_all_fields,
+  products.*,
+  pLOG.created_at as productLogCreatedAt
 FROM reserved_qty_log pLOG
-  INNER JOIN products prod
-    ON log.variation_id = prod.variation_id
+  INNER JOIN products
+    ON pLOG.variation_id = products.variation_id
   INNER JOIN order_contents
-    ON prod.id = order_contents.product_id
+    ON products.id = order_contents.product_id
   INNER JOIN orders
     ON orders.id = order_contents.order_id  ";
 	}	
@@ -33,15 +31,15 @@ FROM reserved_qty_log pLOG
 	public static function queryWhere(  ){
 		
 		return "  WHERE pLOG.id IN(SELECT
-                  MAX(id)
-                FROM reserved_qty_log
-                GROUP BY variation_id)
-    AND prod.id IS NOT NULL
-    AND prod.is_reserved = 1  ";
+                   MAX(id)
+                 FROM reserved_qty_log
+                 GROUP BY variation_id)
+    AND products.id IS NOT NULL
+    AND products.is_reserved = 1  ";
 	}
 	
 	public static function queryGroup(){
-		return " GROUP BY prod.vendor_description, prod.sku, prod.vendor_id, prod.case_price  ";
+		return " GROUP BY products.vendor_description, products.sku, products.vendor_id, products.case_price  ";
 	}
 
 }
