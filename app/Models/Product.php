bfa63a4b235f0240caa6ed87958ab1a3306b277f
Hamid Raza <dev3@shayansolutions.com>
2018-04-04 16:44:24 +0500

    Bug 241 - Reserved Product Quantities

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -429,6 +429,7 @@ FROM `products`
         if (empty($attributes)) {
             return false;
         }
+        $this->update(["inactive"=>$attributes['inactive']]);
         $items = $this->getProductVariations();
         foreach ($items as $item) {
             $updates = $attributes;
