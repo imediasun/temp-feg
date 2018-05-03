07ade57832d5e138ebad4d8ef263848626abbab0
Ayesha <dev6@shayansolutions.com>
2018-05-03 12:02:58 +0500

    bug-305(search by sku)

--- app/Http/Controllers/ManagefegrequeststoreController.php
+++ app/Http/Controllers/ManagefegrequeststoreController.php
@@ -244,7 +244,7 @@ class ManagefegrequeststoreController extends Controller
                 $searchQuery = explode(":", $searchQuery);
                 if ($searchQuery[0] == 'description') {
                     $searchQuery = !empty($searchQuery[2]) ? explode("|", $searchQuery[2])[0] : '';
-                    $filter = $this->getSearchFilterQuery(['query' => $searchQuery, 'fields' => ['products.vendor_description', 'u1.username']]);
+                    $filter = $this->getSearchFilterQuery(['query' => $searchQuery, 'fields' => ['products.vendor_description', 'u1.username','products.sku']]);
                 }
 
                 $searchQueryArray = $this->model->getSearchQueryStringToArray($request->input('search'));
