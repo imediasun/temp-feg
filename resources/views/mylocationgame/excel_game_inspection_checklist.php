7463b8463ee90eaef3d0b5257cda6713e79f7e0f
Ayesha <dev6@shayansolutions.com>
2018-05-24 16:18:00 +0500

    bug-348 chunk size resolved

--- resources/views/mylocationgame/excel_game_inspection_checklist.php
+++ resources/views/mylocationgame/excel_game_inspection_checklist.php
@@ -7,7 +7,7 @@ $pages = 0;
 $offset = .70;
 
 $content .= '<table border="1">';
-foreach ($rows->chunk(6) as $chunk)
+foreach (array_chunk($rows,6)as $chunk)
 {
     $pages++;
     $content .= '<tr>';
