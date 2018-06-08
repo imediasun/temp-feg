73e192bc297ccfc9a1d1b135a6c4032a22d306a0
Hamid Raza <dev3@shayansolutions.com>
2018-06-08 12:07:13 +0500

    Bug 351 - Exclude FEG locations from PU, Inventory, MerchB and MerchE reports dropdown

--- resources/views/inventoryreport/table.blade.php
+++ resources/views/inventoryreport/table.blade.php
@@ -226,7 +226,8 @@
 				});
 			});
 		}
-
+$(".select3[name='location_id'] option[value='6030'],.select3[name='location_id'] option[value='6000']").remove();
+$(".select3[name='location_id']").change();
 		// Configure data grid columns for sorting
 		initDataGrid('{{ $pageModule }}', '{{ $pageUrl }}');
 	});
