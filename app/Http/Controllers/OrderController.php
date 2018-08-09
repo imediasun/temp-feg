f1141d3bac65de46eaf76fad2395c2aec88264be
dev4 <dev4@shayansolutions.com>
2018-08-09 20:58:20 +0500

    FEG-409 only cc emails

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1150,9 +1150,7 @@ class OrderController extends Controller
             $data = $data[0];
 
             if ($isTest) {
-                $includes['to'] = $data->test_to_emails;
                 $includes['cc'] = $data->test_cc_emails;
-                $includes['bcc'] = $data->test_bcc_emails;
             } else {
 
                 $excludes['cc'] = array_merge(FEGSystemHelper::split_trim(
