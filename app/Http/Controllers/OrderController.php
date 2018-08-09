cb7cd434e1b6f9f97eaf666c77aac5165c799668
dev4 <dev4@shayansolutions.com>
2018-08-09 16:27:18 +0500

    FEG-409 removing empty spaces

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1114,6 +1114,11 @@ class OrderController extends Controller
 
             $emailsToArray = array_unique($emailsToArray);
 
+
+            $emailsToArray = array_filter($emailsToArray, function($val){
+                return ($val != '');
+            });
+
             $emailsTo = implode(',', $emailsToArray);
 
             \Session::put('send_to', $emailsTo);
