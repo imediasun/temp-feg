a7aa52fb87ddcd9e93256985ac5639d6f05df26a
dev4 <dev4@shayansolutions.com>
2018-08-13 19:48:03 +0500

    FEG-832-extended adding br tags

--- resources/views/emails/inquireOrder.blade.php
+++ resources/views/emails/inquireOrder.blade.php
@@ -1,18 +1,18 @@
-Location Name/ID: {{$order->location ? $order->location->location_name : ''}},<\br>
-PO number: {{$order->po_number}},<\br>
-Vendor Name: {{$order->vendor ? $order->vendor->vendor_name : ''}},<\br>
-Order Total: {{$order->order_total}}<\br>
+Location Name/ID: {{$order->location ? $order->location->location_name : ''}},<br/>
+PO number: {{$order->po_number}},<br/>
+Vendor Name: {{$order->vendor ? $order->vendor->vendor_name : ''}},<br/>
+Order Total: {{$order->order_total}}<br/>
 
-The details of Items Ordered are<\br>
----------------------------------------------<\br>
----------------------------------------------<\br>
+The details of Items Ordered are<br/>
+---------------------------------------------<br/>
+---------------------------------------------<br/>
 
 @foreach($order->contents as $key=>$content)
 Item# {{$key++}}
 Item name: {{$content->item_name}},
-Item Sku: {{$content->sku}},<\br>
+Item Sku: {{$content->sku}},<br/>
 Case price: {{$content->case_price}},
 Unit Price: {{$content->unit_price}},
 Quantity: {{$content->qty}}
----------------------------------------------<\br>
+---------------------------------------------<br/>
 @endforeach
