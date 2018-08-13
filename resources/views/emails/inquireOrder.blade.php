7db2fb50215c3335629b6013bdb155ae7a70c758
dev4 <dev4@shayansolutions.com>
2018-08-13 19:35:15 +0500

    FEG-832-extended line breaks

--- resources/views/emails/inquireOrder.blade.php
+++ resources/views/emails/inquireOrder.blade.php
@@ -1,18 +1,18 @@
-Location Name/ID: {{$order->location ? $order->location->location_name : ''}},
-PO number: {{$order->po_number}},
-Vendor Name: {{$order->vendor ? $order->vendor->vendor_name : ''}},
-Order Total: {{$order->order_total}}
+Location Name/ID: {{$order->location ? $order->location->location_name : ''}},\r\n
+PO number: {{$order->po_number}},\r\n
+Vendor Name: {{$order->vendor ? $order->vendor->vendor_name : ''}},\r\n
+Order Total: {{$order->order_total}}\r\n
 
-The details of Items Ordered are<\br>
----------------------------------------------<\br>
----------------------------------------------<\br>
+The details of Items Ordered are\r\n
+---------------------------------------------\r\n
+---------------------------------------------\r\n
 
 @foreach($order->contents as $key=>$content)
 Item# {{$key++}}
 Item name: {{$content->item_name}},
-Item Sku: {{$content->sku}},<\br>
+Item Sku: {{$content->sku}},\r\n
 Case price: {{$content->case_price}},
 Unit Price: {{$content->unit_price}},
 Quantity: {{$content->qty}}
----------------------------------------------<\br>
+---------------------------------------------\r\n
 @endforeach
