6ac50a282e836b89aa5af3ab10e2103400aedc04
dev4 <dev4@shayansolutions.com>
2018-08-13 19:56:33 +0500

    FEG-832-extended correcting data format

--- resources/views/emails/inquireOrder.blade.php
+++ resources/views/emails/inquireOrder.blade.php
@@ -112,18 +112,18 @@
 Location Name/ID: {{$order->location ? $order->location->location_name : ''}},<br/>
 PO number: {{$order->po_number}},<br/>
 Vendor Name: {{$order->vendor ? $order->vendor->vendor_name : ''}},<br/>
-Order Total: {{$order->order_total}}<br/>
+Order Total: $ {{$order->order_total}}<br/>
 
 The details of Items Ordered are<br/>
 ---------------------------------------------<br/>
 ---------------------------------------------<br/>
 
 @foreach($order->contents as $key=>$content)
-Item# {{$key++}}
+Item# {{$key++}}<br/>
 Item name: {{$content->item_name}},
 Item Sku: {{$content->sku}},<br/>
-Case price: {{$content->case_price}},
-Unit Price: {{$content->price}},
+Case price: $ {{$content->case_price}},
+Unit Price: $ {{$content->price}},
 Quantity: {{$content->qty}}<br/>
 ---------------------------------------------<br/>
 @endforeach
