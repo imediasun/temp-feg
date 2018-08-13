74c9ba7f08e44ffb42e0fc4d80dd38218b042b47
dev4 <dev4@shayansolutions.com>
2018-08-13 19:28:07 +0500

    FEG-832-extended removed commented code and showing success message after sending email

--- resources/views/emails/inquireOrder.blade.php
+++ resources/views/emails/inquireOrder.blade.php
@@ -3,16 +3,16 @@ PO number: {{$order->po_number}},
 Vendor Name: {{$order->vendor ? $order->vendor->vendor_name : ''}},
 Order Total: {{$order->order_total}}
 
-The details of Items Ordered are
----------------------------------------------
----------------------------------------------
+The details of Items Ordered are<\br>
+---------------------------------------------<\br>
+---------------------------------------------<\br>
 
 @foreach($order->contents as $key=>$content)
 Item# {{$key++}}
 Item name: {{$content->item_name}},
-Item Sku: {{$content->sku}},
+Item Sku: {{$content->sku}},<\br>
 Case price: {{$content->case_price}},
 Unit Price: {{$content->unit_price}},
 Quantity: {{$content->qty}}
----------------------------------------------
+---------------------------------------------<\br>
 @endforeach
