bde0943f28a07625ce198bc7daf018bc5bdd8ee4
dev4 <dev4@shayansolutions.com>
2018-08-18 16:08:40 +0500

    FEG-832 correcting indentation

--- resources/views/order/table.blade.php
+++ resources/views/order/table.blade.php
@@ -291,12 +291,12 @@ usort($tableGrid, "SiteHelpers::_sort");
                                 <i class="fa fa-refresh " aria-hidden="true"></i>
                             </a>
                         @endif
-                            <a href="{{ $pageModule }}/inquire/{{$row->id}}/order"
-                               data-id="{{$eid}}"
-                               class="tips btn btn-xs btn-white inquireOrderAction"
-                               title="{{ Lang::get('core.order_inquiry_button_title') }}">
-                                <i class="fa fa-question" aria-hidden="true"></i>
-                            </a>
+                        <a href="{{ $pageModule }}/inquire/{{$row->id}}/order"
+                           data-id="{{$eid}}"
+                           class="tips btn btn-xs btn-white inquireOrderAction"
+                           title="{{ Lang::get('core.order_inquiry_button_title') }}">
+                            <i class="fa fa-question" aria-hidden="true"></i>
+                        </a>
 					</td>
                 </tr>
 
