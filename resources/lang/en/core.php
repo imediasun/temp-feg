323c3a85a426336ad8e039ecd5abf6c9ca15dac3
dev4 <dev4@shayansolutions.com>
2018-08-27 13:05:54 +0500

    FEG-1241 Added the text above the original text of email

--- resources/lang/en/core.php
+++ resources/lang/en/core.php
@@ -250,7 +250,8 @@ return array(
 
     "reports" => [
         "daily_pending_order_receipt_report" => [
-            "head" => "<p>The following Purchase Orders need to be received in the FEG Admin.</p> <br/>",
+            "head" => "<b>If any of the Purchase Order numbers mentioned in this email have not yet been received, please disregard that Purchase Order number.</b> <br/>
+                        <p>The following Purchase Orders need to be received in the FEG Admin.</p> <br/>",
             "foot" => "<br/> <p>To Receive an Order:</p>
                 <ol>
                     <li>Log into the FEG Admin.</li>
