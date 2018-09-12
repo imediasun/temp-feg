a3d809c6cd2280de52802bf46e17e3f022ef0c8b
dev4 <dev4@shayansolutions.com>
2018-09-12 17:18:11 +0500

    FEG-1517 checking the to, message, subject and options in both methods saveorsendemail and inquireorder

--- app/Library/FEG/System/FEGSystemHelper.php
+++ app/Library/FEG/System/FEGSystemHelper.php
@@ -658,7 +658,7 @@ class FEGSystemHelper
                         );
                         \Config::set('mail', $config);
                         $from = env('MAIL_MERCH_USERNAME');
-                        dd(\Config::get('mail'));
+                        dd([$to, $subject, $message, $from, $options]);
                     }
 
                     return self::laravelMail($to, $subject, $message, $from, $options);
