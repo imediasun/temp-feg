9d770f2a877642639d824c41a70f66ae30ea1994
Hamid Raza <dev3@shayansolutions.com>
2018-09-19 10:52:04 +0500

    FEG-471 Product by Location Grouping [Database Design]

---
+++ app/Models/CustemRelation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustemRelation extends Sximo
+{
+    protected $table = 'custem_relations';
+    protected $primaryKey = 'id';
+    protected $fillable = ['related_id','related_to','related_type','related_type_to','is_excluded'];
+    //
+}
