b444db2e7b7a31a37e40a491aa6f2ecb53a85338
Hamid Raza <dev3@shayansolutions.com>
2018-09-19 17:44:10 +0500

    FEG-864 Add Readers Not Played to Games Summary Report

--- app/Models/Reader.php
+++ app/Models/Reader.php
@@ -16,12 +16,66 @@ class Reader extends Sximo
      */
     public static function getAllReaders()
     {
-        $readers = self::select('reader_id', 'game_id', 'location_id', 'reporting_reader_log', 'date_added')
+        $readers = self::select('reader_id', 'game_id', 'location_id','first_report_date','last_report_date', 'reporting_reader_log', 'date_added')
             ->where("reporting_reader_log", '=', '1')
             ->groupby('reader_id')
             ->groupby('game_id')
             ->groupby('location_id')->get();
         return $readers;
+    }
+
+    /**
+     * @param $gameReaders
+     */
+    public static function updateReaders($gameReaders){
+
+        $reader = self::where("location_id",$gameReaders->location_id);
+        $readers = $reader->where('game_id',$gameReaders->game_id)->where("reader_id",$gameReaders->reader_id)->first();
+        if($readers){
+            $readers->last_report_date=date('Y-m-d',strtotime($gameReaders->date_start));
+            $readers->save();
+        }else{
+            $readerData= [
+                'game_id'=>$gameReaders->game_id,
+                'location_id'=>$gameReaders->location_id,
+                'reader_id'=>$gameReaders->reader_id,
+                'first_report_date'=>date('Y-m-d'),
+                'last_report_date'=> date('Y-m-d',strtotime($gameReaders->date_start)),
+                'reporting_reader_log'=>1
+            ];
+            $reader = new self();
+            $reader->insertRow($readerData,0);
+            $game = game::where('id',$gameReaders->game_id);
+            $gameRecord = $game->where('location_id',$gameReaders->location_id)->first();
+
+            if($gameRecord) {
+                $gameRecord->total_readers = $gameRecord->total_readers+1;
+                $gameRecord->save();
+            }
+        }
+    }
+
+    /**
+     * @param array $params
+     * @return mixed
+     */
+    public static function getReaderNotPlayed($params = [])
+    {
+        extract(array_merge(array(
+            'date' => date('Y-m-d', strtotime('-1 day')),
+        ), $params));
+        $selectColumns = ' group_concat(readers.reader_id) as reader_id,
+         count(readers.reader_id) as total_reader_not_reporing,readers.game_id,readers.location_id,location.location_name,
+         game.game_name,game_title.game_title, if( game.game_title_id > 0,game_title.game_title,game.game_name) as gameTitle';
+        $readers = self::select(\DB::raw($selectColumns))->where('last_report_date', '<', $date);
+
+        $readers->leftJoin('game', 'game.id', '=', 'readers.game_id');
+        $readers->leftJoin('game_title', 'game_title.id', '=', 'game.game_title_id');
+        $readers->leftJoin('location', 'location.id', '=', 'readers.location_id');
+
+        $readerData = $readers->where(\DB::raw('Year(readers.date_added)'), ">=", '2018')
+            ->groupby('readers.game_id')->groupby('readers.location_id')->whereNotNull('game_title.game_title')->get();
+        return $readerData;
     }
 
 }
