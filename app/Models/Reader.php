450a0bec81ccdcb9f8473c791d8993ba03dc14f7
Hamid Raza <dev3@shayansolutions.com>
2018-09-25 11:27:23 +0500

    FEG-864 Add Readers Not Played to Games Summary Report

--- app/Models/Reader.php
+++ app/Models/Reader.php
@@ -74,6 +74,9 @@ class Reader extends Sximo
         $readers->leftJoin('location', 'location.id', '=', 'readers.location_id');
 
         $readerData = $readers->where(\DB::raw('Year(readers.date_added)'), ">=", '2018')
+            ->whereNotNull('game.total_readers')
+            ->where('game.total_readers','>',0)
+            ->having('total_reader_reported','>',0)
             ->having('game.total_readers','>','total_reader_reported')
             ->groupby('readers.game_id')->groupby('readers.location_id')->whereNotNull('game_title.game_title')->get();
         return $readerData;
