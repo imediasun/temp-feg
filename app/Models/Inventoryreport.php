fbcf4e723c1e41c2c5d174ae04214978644d6ec4
dev4 <dev4@shayansolutions.com>
2018-10-02 17:44:17 +0500

    FEG-471 Removing exclusions from InventoryReports

--- app/Models/Inventoryreport.php
+++ app/Models/Inventoryreport.php
@@ -219,21 +219,21 @@ class inventoryreport extends Sximo  {
                 $closeOrderStatus = implode(',',$closeOrderStatus);
             }
 
-            $excludedProductsAndTypes = FEGDBRelationHelpers::getExcludedProductTypeAndExcludedProductIds($location_id);
-            $excludedProductTypeIdsString   = implode(',', $excludedProductsAndTypes['excluded_product_type_ids']);
-            $excludedProductIdsString       = implode(',', $excludedProductsAndTypes['excluded_product_ids']);
-
-            $whereNotInProductTypeAndProductIds = '';
-
-            if($excludedProductTypeIdsString != '')
-                $whereNotInProductTypeAndProductIds .= " AND P.prod_type_id NOT IN($excludedProductTypeIdsString) ";
-
-            if($excludedProductIdsString != '')
-                $whereNotInProductTypeAndProductIds .= " AND (P.id NOT IN($excludedProductIdsString)) ";
+//            $excludedProductsAndTypes = FEGDBRelationHelpers::getExcludedProductTypeAndExcludedProductIds($location_id);
+//            $excludedProductTypeIdsString   = implode(',', $excludedProductsAndTypes['excluded_product_type_ids']);
+//            $excludedProductIdsString       = implode(',', $excludedProductsAndTypes['excluded_product_ids']);
+//
+//            $whereNotInProductTypeAndProductIds = '';
+//
+//            if($excludedProductTypeIdsString != '')
+//                $whereNotInProductTypeAndProductIds .= " AND P.prod_type_id NOT IN($excludedProductTypeIdsString) ";
+//
+//            if($excludedProductIdsString != '')
+//                $whereNotInProductTypeAndProductIds .= " AND (P.id NOT IN($excludedProductIdsString)) ";
 
             $whereQuery = " WHERE O.status_id IN ($closeOrderStatus) AND O.created_at >= '$date_start'
                             AND O.created_at <= '$date_end' 
-                             $whereNotInPoNumber $whereLocation $whereNotInProductTypeAndProductIds $whereVendor $whereOrderType $whereProdType $whereProdSubType $typeDisplayOnly ";
+                             $whereNotInPoNumber $whereLocation    $whereVendor $whereOrderType $whereProdType $whereProdSubType $typeDisplayOnly ";
 
             // both group by quires are same
             $groupQuery = " GROUP BY OC.item_name,OC.qty_per_case,order_type";
