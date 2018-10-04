1062b93b47c2bbf9b95fc4147cf7eb258b158032
dev4 <dev4@shayansolutions.com>
2018-10-04 16:59:50 +0500

    FEG-471 showing excluded products and excluded product types on the basis of name and type name ascending order in the listing table of location and locationgroups

--- app/Models/Locationgroups.php
+++ app/Models/Locationgroups.php
@@ -50,18 +50,18 @@ class Locationgroups extends Sximo  {
             $productType = FEGDBRelationHelpers::getCustomRelationRecords($row->id,Ordertyperestrictions::class,self::class,1)->pluck('ordertyperestrictions_id')->toArray();
 
             $locations = location::select(\DB::raw("GROUP_CONCAT(DISTINCT CONCAT(location.id,' ',location.location_name) ORDER BY location.id SEPARATOR '<br>') AS location_names"))->whereIn('id',$locationData)->where('active',1)->get()->pluck('location_names')->toArray();
-            $productTypeData = Ordertyperestrictions::select(\DB::raw('group_concat(order_type) as product_types'))->whereIn('id', $productType)->get()->pluck('product_types')->toArray();
-            $productsData = product::select(\DB::raw('group_concat(vendor_description) as product_name'))->whereIn('id', $productData)->get()->pluck('product_name')->toArray();
+            $productTypeData = Ordertyperestrictions::select(\DB::raw('group_concat(order_type ORDER BY order_type ASC) as product_types'))->whereIn('id', $productType)->get()->pluck('product_types')->toArray();
+            $productsData = product::select(\DB::raw('group_concat(vendor_description ORDER BY vendor_description ASC) as product_name'))->whereIn('id', $productData)->get()->pluck('product_name')->toArray();
             if(!empty($productTypeData[0])){
-                $productTypes = str_replace(",","<br>",$productTypeData[0]);
+                $productTypes = str_replace(",",",<br>",$productTypeData[0]);
                 $row->excluded_product_type_ids = $productTypes;
             }
             if(!empty($productsData[0])){
-                $productName= str_replace(",","<br>",$productsData[0]);
+                $productName= str_replace(",",",<br>",$productsData[0]);
                 $row->excluded_product_ids = $productName;
             }
             if(!empty($locations[0])){
-                $locationName= str_replace(",","<br>",$locations[0]);
+                $locationName= str_replace(",",",<br>",$locations[0]);
                 $row->location_ids = $locationName;
             }
             $returnData[] = $row;
