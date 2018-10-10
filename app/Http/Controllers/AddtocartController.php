86247edbb1da7d1e1b4da4d931961c73c56c3d4d
Hamid Raza <dev3@shayansolutions.com>
2018-10-10 11:33:47 +0500

    FEG-1617 Reserved Product negative quantity request prevention [Error message text has been changed]

--- app/Http/Controllers/AddtocartController.php
+++ app/Http/Controllers/AddtocartController.php
@@ -375,12 +375,12 @@ class AddtocartController extends Controller
         if ($requestQtyCheck->count() > 0) {
             $productsNames = "<ul style='padding-left: 17px;margin-bottom: 0px; text-align:left !important;'>";
             foreach ($requestQtyCheck as $request) {
-                $productsNames .= "<li>" . addslashes($request->vendor_description) . " | Product QTY = ".$request->productQty." | Requested QTY = ".$request->requestedQTY."</li>";
+                $productsNames .= "<li>" . addslashes($request->vendor_description) . " | Reserve Qty: = ".$request->productQty." | Requested Qty = ".$request->requestedQTY."</li>";
             }
             $productsNames .= "</ul>";
             //return redirect('/addtocart')->with('messagetext', "You are unable to submit request as following product(s) doesn't allow the negative reserved quantity: $productsNames Please remove product(s) or adjust quantity before submitting the request.")->with('msgstatus', 'error');
        $qtyCheckMessage = [
-           'messagetext' => "You are unable to submit request as following product(s) doesn't allow the negative reserved quantity:<br /> $productsNames <br />Please remove product(s) or adjust quantity before submitting the request.",
+           'messagetext' => "Your request cannot be submitted because there is not enough reserve qty to allow the purchase.<br /> $productsNames <br />Please reduce the amount requested for purchase below or contact the Merchandise Team.",
             'showError' => $requestQtyCheck->count() > 0,
        ];
         }
