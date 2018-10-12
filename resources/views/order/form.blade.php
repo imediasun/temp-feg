de04d370e52e891d97183b96d55d3f3ce869b9b8
Hamid Raza <dev3@shayansolutions.com>
2018-10-12 18:05:45 +0500

    FEG-471 adding select all option in  dropdowns

--- resources/views/order/form.blade.php
+++ resources/views/order/form.blade.php
@@ -1104,6 +1104,7 @@
         var games_dropdown = [];
         oldLocationId   = null;
         oldAltShipToVal = null;
+
         $("#location_id").click(function () {
             changeLocation();
         });
@@ -1210,8 +1211,12 @@
 
                 },
                 cancel:function(){
-                    $('#location_id option[value = '+oldLocationId+']').attr('selected', 'selected');
-                    $('#select2-chosen-7').html($("#location_id option:selected").text());
+                    @if($mode == 'clone')
+                    oldLocationId = {{ $data['order_loc_id'] }}
+                    @endif
+
+                    $('select#location_id').select2('val', [oldLocationId]);
+
                     if(oldAltShipToVal){
                         $('#alt_ship_to').attr('checked', true);
                         $('#alt_ship_to').parent().addClass('checked');
