bcf83fa7759bc25bf85f6a579c432a3d3074aafe
Hamid Raza <dev3@shayansolutions.com>
2018-10-29 16:50:01 +0500

    FEG-98 Broken Case Functionality [Remove Order Qty Issue in Log]

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -127,6 +127,7 @@ class order extends Sximo
         if(!empty($pass['calculate price according to case price'])) {
             $order_types = explode(',',$pass['calculate price according to case price']->data_options);
         }
+
         foreach ($orderContents as $orderContent) {
 
             $orderedProduct = $orderContent->product;
@@ -138,12 +139,12 @@ class order extends Sximo
                     if ($orderedProduct->allow_negative_reserve_qty == 0 && $orderedProduct->reserved_qty < $orderContent->qty) {
                         throw new \Exception("Product does not have sufficient reserved quantities");
                     }
-                    $reserved_qty =  ($orderContent->is_broken_case == 1 && !in_array($this->order_type_id,$order_types)) ? ($orderedProduct->reserved_qty - ceil($orderContent->qty/$orderContent->qty_per_case)):$orderedProduct->reserved_qty - $orderContent->qty;
+                    $reserved_qty =  ($orderContent->is_broken_case == 1 && in_array($this->order_type_id,$order_types)) ? ($orderedProduct->reserved_qty - ceil($orderContent->qty/$orderContent->qty_per_case)):$orderedProduct->reserved_qty - $orderContent->qty;
 
                     $reservedLogData = [
                         "product_id" => $orderContent->product_id,
                         "order_id" => $orderContent->order_id,
-                        "adjustment_amount" => ($orderContent->is_broken_case == 1 && !in_array($this->order_type_id,$order_types)) ? ceil($orderContent->qty/$orderContent->qty_per_case):$orderContent->qty,
+                        "adjustment_amount" => ($orderContent->is_broken_case == 1 && in_array($this->order_type_id,$order_types)) ? ceil($orderContent->qty/$orderContent->qty_per_case):$orderContent->qty,
                         "variation_id"=>$orderedProduct->variation_id,
                         "adjustment_type" => "negative",
                         "adjusted_by" => \AUTH::user()->id,
@@ -163,12 +164,12 @@ class order extends Sximo
                 {
                     //This part is all working
                     Log::info("Putting back qty to product because order is deleting");
-                    $reserved_qty =  ($orderContent->is_broken_case == 1 && !in_array($this->order_type_id,$order_types)) ? ($orderedProduct->reserved_qty + ceil($orderContent->qty/$orderContent->qty_per_case)):$orderedProduct->reserved_qty + $orderContent->qty;
+                    $reserved_qty =  ($orderContent->is_broken_case == 1 && in_array($this->order_type_id,$order_types)) ? ($orderedProduct->reserved_qty + ceil($orderContent->qty/$orderContent->qty_per_case)):$orderedProduct->reserved_qty + $orderContent->qty;
 
                     $reservedLogData = [
                         "product_id" => $orderContent->product_id,
                         "order_id" => $orderContent->order_id,
-                        "adjustment_amount" => ($orderContent->is_broken_case == 1 && !in_array($this->order_type_id,$order_types)) ? ceil($orderContent->qty/$orderContent->qty_per_case):$orderContent->qty,
+                        "adjustment_amount" => ($orderContent->is_broken_case == 1 && in_array($this->order_type_id,$order_types)) ? ceil($orderContent->qty/$orderContent->qty_per_case):$orderContent->qty,
                         "variation_id"=>$orderedProduct->variation_id,
                         "adjustment_type" => "positive",
                         "adjusted_by" => \AUTH::user()->id,
