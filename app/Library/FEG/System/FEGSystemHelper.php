97e8b9eed5ad2cdd00881d732d602403875c39a1
dev4 <dev4@shayansolutions.com>
2018-11-08 19:58:38 +0500

    FEG-832 merging the popup emails and config emails in case of InquireOrder

--- app/Library/FEG/System/FEGSystemHelper.php
+++ app/Library/FEG/System/FEGSystemHelper.php
@@ -1255,14 +1255,29 @@ $message" .
             $options['message'] = $message;
             $options['subject'] = $subject = "[TEST] " . $subject;
             $emailRecipients = self::getSystemEmailRecipients($configName, null, true);
-            if(overrideToEmailInTestMode){
-                $options['to'] = $to = array_merge($to,$emailRecipients['to']);
+
+            if($overrideToEmailInTestMode){
+                $options['to'] = $to = self::mergeTheConfigEmailAndPopUpEmails($to, $emailRecipients['to']);
             }
             else{
                 $options['to'] = $to = $emailRecipients['to'];
             }
-            $options['cc'] = $cc = $emailRecipients['cc'];
-            $options['bcc'] = $bcc = $emailRecipients['bcc'];
+
+            if($overrideCCEmailInTestMode){
+                $options['cc'] = $cc = self::mergeTheConfigEmailAndPopUpEmails($cc, $emailRecipients['cc']);
+            }
+            else{
+                $options['cc'] = $cc = $emailRecipients['cc'];
+            }
+
+
+            if($overrideBCCEmailInTestMode){
+                $options['bcc'] = $bcc = self::mergeTheConfigEmailAndPopUpEmails($bcc, $emailRecipients['bcc']);
+            }
+            else{
+                $options['bcc'] = $bcc = $emailRecipients['bcc'];
+            }
+
             if (empty($to)) {
                 $to = "e5devmail@gmail.com";
             }
@@ -1288,6 +1303,21 @@ $message" .
         self::logit("Email sent Status = " . $status, $lf, $lp);
         self::logit("Email sent", $lf, $lp);
         return $status;
+    }
+
+    public static function mergeTheConfigEmailAndPopUpEmails($popUpEmails, $configEmails){
+        $popUpEmailsArray = explode(',', $popUpEmails);
+        $emailRecipientsArray = explode(',', $configEmails);
+        $finalArray = array_merge($popUpEmailsArray,$emailRecipientsArray);
+        $finalArray = array_unique($finalArray);
+        $finalArray = array_filter($finalArray, function($entry){
+            return ($entry != '') ? true : false;
+        });
+        return $finalEmailsString = implode(',', $finalArray);
+    }
+
+    function remove_empty_entry($entry){
+        return ($entry != '') ? true : false;
     }
 
     public static function getOption($optionName, $default = '', $all = false, $skipInactive = false, $details = false)
