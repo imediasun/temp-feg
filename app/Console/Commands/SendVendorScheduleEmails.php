39f0ef5080edb54f03bbc06f2c38e0c9b68c5283
Hamid Raza <dev3@shayansolutions.com>
2018-11-14 17:53:45 +0500

    FEG-503 Vendor Product Import

--- app/Console/Commands/SendVendorScheduleEmails.php
+++ app/Console/Commands/SendVendorScheduleEmails.php
@@ -79,24 +79,25 @@ class SendVendorScheduleEmails extends Command
         //Schedule for all
         $vendors = Vendor::whereNotIn('id', $vendorIds)->get();//get those vendors who don't have schedule
         $schedule = VendorImportSchedule::where('vendor_id', 0)->first();
-        foreach ($vendors as $vendor){
-            $vendorEmail = $vendor->email ? $vendor->email:$vendor->email_2;
-            //If vendor emails are not empty
-            if($vendorEmail != '') {
-                //If email schedule weekly
-                if ($schedule->reoccur_by == 'weekly') {
-                    $days = explode(',', $schedule->days);
-                    foreach ($days as $day) {
-                        //Check if schedule day is equal to current day.
-                        if ($day == lcfirst($currentTime->format('l'))) {
+        if($schedule) {
+            foreach ($vendors as $vendor) {
+                $vendorEmail = $vendor->email ? $vendor->email : $vendor->email_2;
+                //If vendor emails are not empty
+                if ($vendorEmail != '') {
+                    //If email schedule weekly
+                    if ($schedule->reoccur_by == 'weekly') {
+                        $days = explode(',', $schedule->days);
+                        foreach ($days as $day) {
+                            //Check if schedule day is equal to current day.
+                            if ($day == lcfirst($currentTime->format('l'))) {
+                                VendorProductsImportHelper::exportExcel($vendor->id, $vendorEmail);
+                            }
+                        }
+                    } //If email schedule monthly
+                    else {
+                        if ($schedule->date == $currentTime->format('d')) {
                             VendorProductsImportHelper::exportExcel($vendor->id, $vendorEmail);
                         }
-                    }
-                }
-                //If email schedule monthly
-                else {
-                    if ($schedule->date == $currentTime->format('d')) {
-                        VendorProductsImportHelper::exportExcel($vendor->id, $vendorEmail);
                     }
                 }
             }
