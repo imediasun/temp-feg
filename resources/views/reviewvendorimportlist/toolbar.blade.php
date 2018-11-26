ee32c95232b44f9d23bef010dfc8eeca369cb246
Dev3 <marslan.ali@gmail.com>
2018-11-26 18:44:31 +0500

    FEG-503, Need to remove CSV button and add PRINT on vendor import list module

--- resources/views/reviewvendorimportlist/toolbar.blade.php
+++ resources/views/reviewvendorimportlist/toolbar.blade.php
@@ -39,7 +39,7 @@
 
     </div>
 	<div class="col-md-3 ">
-        <?php 
+        <?php
             $isExcel = isset($access['is_excel']) && $access['is_excel'] == 1;
             $isCSV = isset($access['is_csv'])  ? ($access['is_csv'] == 1) : $isExcel;
             $isPDF = isset($access['is_pdf'])  && $access['is_pdf'] == 1;
@@ -48,23 +48,23 @@
             $isExport = $isExcel || $isCSV || $isPDF || $isWord || $isPrint;
         ?>
 		@if($isExport)
-		<div class="pull-right">
-            @if($isExcel)
-			<a href="{{ URL::to( $pageModule .'/export/excel?exportID='.uniqid('excel', true).'&return='.$return) }}" class="btn btn-sm btn-white"> Excel</a>
-			@endif
-            @if($isCSV)
-			<a href="{{ URL::to( $pageModule .'/export/csv?exportID='.uniqid('csv', true).'&return='.$return) }}" class="btn btn-sm btn-white"> CSV </a>
-			@endif
-            @if($isPDF)
-			<a href="{{ URL::to( $pageModule .'/export/pdf?exportID='.uniqid('pdf', true).'&return='.$return) }}" class="btn btn-sm btn-white"> PDF</a>
-			@endif
-            @if($isWord)
-			<a href="{{ URL::to( $pageModule .'/export/word?exportID='.uniqid('word', true).'&return='.$return) }}" class="btn btn-sm btn-white"> Word</a>
-			@endif
-            @if($isPrint)
-			<a href="{{ URL::to( $pageModule .'/export/print?exportID='.uniqid('print', true).'&return='.$return) }}" class="btn btn-sm btn-white" onclick="ajaxPopupStatic(this.href); return false;" > Print</a>
-			@endif
-		</div>	
+		{{--<div class="pull-right">--}}
+            {{--@if($isExcel)--}}
+			{{--<a href="{{ URL::to( $pageModule .'/export/excel?exportID='.uniqid('excel', true).'&return='.$return) }}" class="btn btn-sm btn-white"> Excel</a>--}}
+			{{--@endif--}}
+            {{--@if($isCSV)--}}
+			{{--<a href="{{ URL::to( $pageModule .'/export/csv?exportID='.uniqid('csv', true).'&return='.$return) }}" class="btn btn-sm btn-white"> CSV </a>--}}
+			{{--@endif--}}
+            {{--@if($isPDF)--}}
+			{{--<a href="{{ URL::to( $pageModule .'/export/pdf?exportID='.uniqid('pdf', true).'&return='.$return) }}" class="btn btn-sm btn-white"> PDF</a>--}}
+			{{--@endif--}}
+            {{--@if($isWord)--}}
+			{{--<a href="{{ URL::to( $pageModule .'/export/word?exportID='.uniqid('word', true).'&return='.$return) }}" class="btn btn-sm btn-white"> Word</a>--}}
+			{{--@endif--}}
+            {{--@if($isPrint)--}}
+			{{--<a href="{{ URL::to( $pageModule .'/export/print?exportID='.uniqid('print', true).'&return='.$return) }}" class="btn btn-sm btn-white" onclick="ajaxPopupStatic(this.href); return false;" > Print</a>--}}
+			{{--@endif--}}
+		{{--</div>--}}
 		@endif
 	</div>
 </div>
