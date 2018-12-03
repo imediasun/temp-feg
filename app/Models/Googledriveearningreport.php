758052693fa3b564f5300b5bafa4ba3e0b82043e
waqasahmad.codingpixel <waqasahmad.codingpixel@gmail.com>
2018-12-03 17:09:36 +0500

    FEG-985, get all files from google drive

--- app/Models/Googledriveearningreport.php
+++ app/Models/Googledriveearningreport.php
@@ -13,6 +13,27 @@ class googledriveearningreport extends Sximo  {
 		
 	}
 
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+                        'google_file_id',
+                        'file_name',
+                        'web_view_link',
+                        'icon_link',
+                        'modified_time',
+                        'created_time',
+                        'mime_type',
+                        'parent_id',
+                        'location_name',
+                        'loc_id',
+                        'path',
+                    ];
+
 	public static function querySelect(  ){
 		
 		return "  SELECT google_drive_earning_reports.* FROM google_drive_earning_reports  ";
@@ -26,5 +47,24 @@ class googledriveearningreport extends Sximo  {
 	public static function queryGroup(){
 		return "  ";
 	}
+
+	public function createOrUpdateFile($file, $location, $path)
+    {
+        $loc = explode('-',$location);
+        $saved = $this->updateOrcreate(['google_file_id' => $file->id],
+            [
+                'file_name' => $file->name,
+                'web_view_link' => $file->webViewLink,
+                'icon_link' => $file->iconLink,
+                'modified_time' => $file->modifiedTime,
+                'created_time' =>$file->createdTime,
+                'mime_type' =>$file->mimeType,
+                'parent_id' =>$file->parents[0],
+                'location_name' => $loc[1],
+                'loc_id' => $loc[0],
+                'path'=>$path,
+            ]);
+        return $saved;
+    }
 
 }
