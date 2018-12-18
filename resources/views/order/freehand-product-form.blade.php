360b93d4ff98dcc55f95e80a7354ecdd9f1a5315
Hamid Raza <dev3@shayansolutions.com>
2018-12-18 15:14:54 +0500

    FEG-487 Add Freehand Product from Order to Product List

--- resources/views/order/freehand-product-form.blade.php
+++ resources/views/order/freehand-product-form.blade.php
@@ -55,7 +55,7 @@
 
         <div class="sbox-content">
             @endif
-            @include('product.form.form',['actionUrl' => $actionUrl, 'fields' => $fields, 'row' => $row, 'variations' => $variations, 'productTypes' => $productTypes, 'id' => $id,'pageModule' => $pageModule, 'fromOrder' => $fromOrder, 'rowId' => $rowId ])
+            @include('product.form.form',['actionUrl' => $actionUrl, 'fields' => $fields, 'row' => $row, 'variations' => $variations, 'productTypes' => $productTypes, 'id' => $id,'pageModule' => $pageModule, 'fromOrder' => $fromOrder, 'rowId' => $rowId,'itemForm' => 'ItemForm' ])
             @if($setting['form-method'] =='native')
         </div>
     </div>
