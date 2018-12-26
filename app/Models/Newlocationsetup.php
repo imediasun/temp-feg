c09b4199e559130944309da17ea71003f9e28835
awais <dev2@shayansolutions.com>
2018-12-26 17:27:03 +0500

    FEG-937 displaying data according to user assigned locations

--- app/Models/Newlocationsetup.php
+++ app/Models/Newlocationsetup.php
@@ -27,4 +27,143 @@ class newlocationsetup extends Sximo  {
 		return "  ";
 	}
 
+    public static function getRows($args, $cond = null) {
+        $table = with(new static)->table;
+        $key = with(new static)->primaryKey;
+
+        extract(array_merge(array(
+            'page' => '0',
+            'limit' => '0',
+            'sort' => '',
+            'extraSorts' => [],
+            'customSorts' => [],
+            'order' => '',
+            'params' => '',
+            'global' => 1
+        ), $args));
+        $orderConditional1 = '';
+        if (!empty($customSorts)) {
+            $customOrderConditionals = [];
+            foreach($customSorts as $customSort => $customSortType) {
+                $customSortItem = '`'.$customSort.'` '.$customSortType;
+                $customOrderConditionals[] = $customSortItem;
+            }
+            $orderConditional1 = implode(', ', $customOrderConditionals);
+            $orderConditional1 = !empty($orderConditional1) ? $orderConditional1.", ":$orderConditional1;
+        }
+
+        $orderConditional = ($sort != '' && $order != '') ? " ORDER BY {$orderConditional1} {$sort} {$order} " : '';
+        if (!empty($extraSorts)) {
+            if (empty($orderConditional)) {
+                $orderConditional = " ORDER BY ";
+            }
+            else {
+                $orderConditional .= ", ";
+            }
+            $extraOrderConditionals = [];
+            foreach($extraSorts as $extraSortItem) {
+                $extraSortItem[0] = '`'.$extraSortItem[0].'`';
+                $extraOrderConditionals[] = implode(' ', $extraSortItem);
+            }
+            $orderConditional .= implode(', ', $extraOrderConditionals);
+        }
+
+
+
+        // Update permission global / own access new ver 1.1
+        $table = with(new static)->table;
+        if ($global == 0)
+            $params .= " AND {$table}.entry_by ='" . \Session::get('uid') . "'";
+        // End Update permission global / own access new ver 1.1
+
+        $rows = array();
+        $select = self::querySelect();
+
+        /*
+
+        */
+        $createdFlag = false;
+
+        if ($cond != null) {
+            $select .= self::queryWhere($cond);
+        }
+        else {
+            $select .= self::queryWhere();
+        }
+
+        if(!empty($createdFrom)){
+            if($cond != 'only_api_visible')
+            {
+                $select .= " AND created_at BETWEEN '$createdFrom' AND '$createdTo'";
+            }
+            else
+            {
+                $select .= " AND api_created_at BETWEEN '$createdFrom' AND '$createdTo'";
+            }
+            $createdFlag = true;
+        }
+
+        if(!empty($updatedFrom)){
+
+            if($createdFlag){
+                if($cond != 'only_api_visible')
+                {
+                    $select .= " OR updated_at BETWEEN '$updatedFrom' AND '$updatedTo'";
+                }
+                else
+                {
+                    $select .= " OR api_updated_at BETWEEN '$updatedFrom' AND '$updatedTo'";
+                }
+            }
+            else{
+                if($cond != 'only_api_visible')
+                {
+                    $select .= " AND updated_at BETWEEN '$updatedFrom' AND '$updatedTo'";
+                }
+                else
+                {
+                    $select .= " AND api_updated_at BETWEEN '$updatedFrom' AND '$updatedTo'";
+                }
+            }
+
+        }
+
+        if(!empty($order_type_id)){
+            $select .= " AND order_type_id in($order_type_id)";
+        }
+        if(!empty($status_id)){
+            $select .= " AND status_id='$status_id'";
+        }
+        if(!empty($active)){//added for location
+            $select .= " AND location.active='$active'";
+        }
+        $select.=' AND '.$table.'.location_id IN ('.\SiteHelpers::getCurrentUserLocationsFromSession().')';
+        \Log::info("Total Query : ".$select . " {$params} " . self::queryGroup() . " {$orderConditional}");
+        $counter_select =\DB::select($select . " {$params} " . self::queryGroup() . " {$orderConditional}");
+        $total= count($counter_select);
+        if($table=="img_uploads")
+        {
+            $total="";
+        }
+        $offset = ($page - 1) * $limit;
+        if ($offset >= $total && $total != 0 && $limit != 0) {
+            $page = ceil($total/$limit);
+            $offset = ($page-1) * $limit ;
+        }
+
+        $limitConditional = ($page != 0 && $limit != 0) ? "LIMIT  $offset , $limit" : '';
+        // echo $select . " {$params} " . self::queryGroup() . " {$orderConditional}  {$limitConditional} ";
+        \Log::info("Query : ".$select . " {$params} " . self::queryGroup() . " {$orderConditional}  {$limitConditional} ");
+        self::$getRowsQuery = $select . " {$params} " . self::queryGroup() . " {$orderConditional}  {$limitConditional} ";
+        $result = \DB::select($select . " {$params} " . self::queryGroup() . " {$orderConditional}  {$limitConditional} ");
+
+        if ($key == '') {
+            $key = '*';
+        } else {
+            $key = $table . "." . $key;
+        }
+
+        return $results = array('rows' => $result, 'total' => $total);
+    }
+
 }
