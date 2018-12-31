3fcbdd63b49678b10db257d8808b19ff9e19ca20
awais <dev2@shayansolutions.com>
2018-12-31 20:28:45 +0500

    FEG-937 fixing the advance search

--- app/Models/Googledriveearningreport.php
+++ app/Models/Googledriveearningreport.php
@@ -38,8 +38,8 @@ class googledriveearningreport extends Sximo  {
 
 	public static function querySelect(  ){
 		
-		return "SELECT google_drive_earning_reports.*, modified_time as date_start, modified_time as date_end,
-      google_drive_earning_reports.loc_id as location_parent_folder_name FROM google_drive_earning_reports";
+		return "SELECT google_drive_earning_reports.* ,  google_drive_earning_reports.modified_time AS date_start,  google_drive_earning_reports.modified_time AS date_end,  location.gd_parent_folder_name 
+FROM google_drive_earning_reports   LEFT JOIN location  ON google_drive_earning_reports.loc_id = location.id";
 	}	
 
 	public static function queryWhere(  ){
