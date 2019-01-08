45e649ca98888c094bd583bec8e874aca0845d6b
Hamid Raza <dev3@shayansolutions.com>
2019-01-08 17:41:46 +0500

    ENV Configuration Module and Daily Test ENV Configuration script has been created.

--- app/Models/Envconfiguration.php
+++ app/Models/Envconfiguration.php
@@ -27,4 +27,67 @@ class Envconfiguration extends Sximo  {
 		return "  ";
 	}
 
+    /**
+     * @param $key
+     * @return bool
+     */
+    public static function isExtraVariable($key){
+
+        $extraVariables = [
+            "PATH",
+            "TZ",
+            "SCRIPT_NAME",
+            "REQUEST_URI",
+            "QUERY_STRING",
+            "REQUEST_METHOD",
+            "SERVER_PROTOCOL",
+            "GATEWAY_INTERFACE",
+            "REDIRECT_URL",
+            "REMOTE_PORT",
+            "SCRIPT_FILENA",
+            "SERVER_ADMIN",
+            "CONTEXT_DOCUMENT_ROOT",
+            "CONTEXT_PREFIX",
+            "REQUEST_SCHEME",
+            "DOCUMENT_ROOT",
+            "REMOTE_ADDR",
+            "SERVER_PORT",
+            "SERVER_ADDR",
+            "SERVER_NAME",
+            "SERVER_SOFTWARE",
+            "SERVER_SIGNATURE",
+            "LD_LIBRARY_PATH",
+            "HTTP_X_HTTPS",
+            "HTTP_COOKIE",
+            "HTTP_ACCEPT_LANGUAGE",
+            "HTTP_ACCEPT_ENCODING",
+            "HTTP_ACCEPT",
+            "HTTP_USER_AGENT",
+            "HTTP_UPGRADE_INSECURE_REQUESTS",
+            "HTTP_CACHE_CONTROL",
+            "HTTP_PRAGMA",
+            "HTTP_CONNECTION",
+            "HTTP_HOST",
+            "SSL_TLS_SNI",
+            "HTTPS",
+            "SCRIPT_URI",
+            "SCRIPT_URL",
+            "UNIQUE_ID",
+            "REDIRECT_STATUS",
+            "REDIRECT_SSL_TLS_SNI",
+            "REDIRECT_HTTPS",
+            "REDIRECT_SCRIPT_URI",
+            "REDIRECT_SCRIPT_URL",
+            "REDIRECT_UNIQUE_ID",
+            "FCGI_ROLE",
+            "PHP_SELF",
+            "REQUEST_TIME_FLOAT",
+            "REQUEST_TIME",
+            "argv",
+            "argc",
+        ];
+
+        return !in_array($key,$extraVariables);
+
+    }
 }
