e695a031654352cdd535696294422a1f0e6ff134
awais <dev2@shayansolutions.com>
2019-01-18 18:40:11 +0500

    FEG-937 stoping the process  of extracting files for the time being due to huge memory loss on demo enviornment

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -58,12 +58,12 @@ class Kernel extends ConsoleKernel
         $schedule->command('cleanproductmeta')->hourly();
         $schedule->command('checkapi')->hourly();
 
-        $schedule->command('extract:googledrivefiles Daily')->daily();//Get Daily google drive files
-        $schedule->command('extract:googledrivefiles Weekly')->weekly();//Get Daily google drive files
-        $schedule->command('extract:googledrivefiles Monthly')->monthly();//Get Daily google drive files
-        $schedule->command('extract:googledrivefiles 13Weeks')->weekly();//Get Daily google drive files
-
-        $schedule->command('extract:googledrivelocations')->weekly();//Get the google drive location files ID (Daily, weekly, monthly, 13 weeks)
+//        $schedule->command('extract:googledrivefiles Daily')->daily();//Get Daily google drive files
+//        $schedule->command('extract:googledrivefiles Weekly')->weekly();//Get Daily google drive files
+//        $schedule->command('extract:googledrivefiles Monthly')->monthly();//Get Daily google drive files
+//        $schedule->command('extract:googledrivefiles 13Weeks')->weekly();//Get Daily google drive files
+//
+//        $schedule->command('extract:googledrivelocations')->weekly();//Get the google drive location files ID (Daily, weekly, monthly, 13 weeks)
         $schedule->command('refresh:googledriveaccesstoken')->cron('*/55 * * * *');//Refresh Google drive access token.
     }
 }
