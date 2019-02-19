50206e496833ece3689742153c12a6cd8de27c51
Hamid Raza <dev3@shayansolutions.com>
2019-02-19 19:41:25 +0500

    FEG-2905 Vendor Product Import CHANGE: items-not-included

--- app/Library/VendorProductsImportHelper.php
+++ app/Library/VendorProductsImportHelper.php
@@ -81,6 +81,7 @@ class VendorProductsImportHelper
         $data[] = [];
         $data[] = [];
         $data[] = ["Don't update Product ID."];
+        $data[] = ["vendor#".$product->vendor_id];
 
 
         // save each row of the data
