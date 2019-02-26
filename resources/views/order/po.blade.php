f975a4167fa290643d10b0926a678195bc14eed3
awais <dev2@shayansolutions.com>
2019-02-26 11:36:33 +0500

    FEG-3022 fixing empty fed no

--- resources/views/order/po.blade.php
+++ resources/views/order/po.blade.php
@@ -48,8 +48,8 @@
                         <br/>
                         <br/>
                         @if($data[0]['ismerch'] && (in_array($data[0]['order_type_id'], ['6', '7', '8'])))
-                        @elseif($data[0]['is_fed']==1)
-                            {{dd($data[0]['fedex_number'])}}
+                            @endif
+                        @if($data[0]['is_fed']==1)
                             {{ $data[0]['fedex_number'] ?  'Please Ship parcels using '.Lang::get('core.fedex_number') .': '.$data[0]['fedex_number'] : ''}}
                         @endif
                     </td>
