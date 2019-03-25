2e84e7f1ed579c04ce6a790b608e372d613613b4
dev4 <dev1@shayansolutions.com>
2019-03-25 18:19:59 +0500

    FEG-2145 check the isgame and ismerch checkboxes only if config is among ['Request Invoice - Games', 'Request Invoice - Merchandise']

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3750,14 +3750,9 @@ ORDER BY aa_id");
         if(!$order)
             return Response::json(['status'=>'error', 'message'=> \Lang::get('core.note_order_not_found')]);
 
-
         $vendor = $order->vendor;
         if(!$vendor)
             return Response::json(['status'=>'error', 'message'=>"Oops! No Vendor found. Please contact to the administrator."]);
-
-        if(!$vendor->isgame && !$vendor->ismerch)
-            return Response::json(['status'=>'error', 'message'=>"Please update vendor entry and select whether this is a Games Vendor or a Merchandise Vendor."]);
-
 
         $orderType = $order->order_type_id;
 
@@ -3769,6 +3764,9 @@ ORDER BY aa_id");
         $email_senders = [];
         $configNames = ['Request Invoice - Games', 'Request Invoice - Merchandise'];
         if(in_array($config_name, $configNames)){
+
+            if(!$vendor->isgame && !$vendor->ismerch)
+                return Response::json(['status'=>'error', 'message'=>"Please update vendor entry and select whether this is a Games Vendor or a Merchandise Vendor."]);
 
             if($vendor->isgame){
                 $email_senders[] = SystemEmailConfigName::with('email_sender')
