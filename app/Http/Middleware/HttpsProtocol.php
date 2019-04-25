71105d01817de30aaeb93a0c91eb439c6fafb93f
Arslan <shayansolutions@gmail.com>
2019-04-25 15:30:07 +0500

    [HotFix] improving performance by excluding ajax based request

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -19,7 +19,7 @@ class HttpsProtocol
             return $next($request);
         }
 
-        if (!$request->secure()) {
+        if (!$request->secure() && !$request->ajax() ) {
             return redirect()->secure($request->getRequestUri());
         }
         return $next($request);
