1fdd9df5ab59ae904a3a4f210b6bd150acaf3bb9
imediasun <imediasun@gmail.com>
2019-06-19 23:37:41 +0300

    FEGPRJ-739 repear isOrderReceived problem

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1643,10 +1643,12 @@ class order extends Sximo
     return in_array(\Session::get('gid'),$userGroups);
     }
     public function isOrderReceived(){
+        if($this->contents && $this->orderReceived && $this->is_freehand && $this->location){
         if ($this->contents->sum('qty') == $this->orderReceived->sum('quantity') && $this->is_freehand == 0 && in_array($this->location->debit_type_id,location::DEBIT_TYPES)) {
             return  true;
         } else {
             return false;
+        }
         }
     }
 
