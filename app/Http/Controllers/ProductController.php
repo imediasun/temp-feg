325193013d06968e029a2c3122a5c65a1f42f2c1
imediasun <imediasun@gmail.com>
2019-06-20 20:46:35 +0300

    FEGPRJ-738 bug fixes Adding readonly field

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -590,6 +590,8 @@ class ProductController extends Controller
         $variations = [];
         $row = $this->model->find($id);
         $_row = $this->model->getRow($id);
+        $row2=clone $_row;
+        $_row = $this->model->setGroupsAndLocations($row2,true,true);
         $row->excluded_locations_and_groups=$_row->excluded_locations_and_groups;
         $row->product_type_excluded_data=$_row->product_type_excluded_data;
         if ($row) {
